7b78980dfbc59ff6e0bfa85dfe9147c40ea651f6
joaosoliveira0907 <joao.soliveira@bandtec.com.br>
2022-05-28 23:59:20 -0300

    Continuacao de Exportacao de arquivos txt

--- 02_BackEnd/unibook-jpa/src/main/java/com/sptech/unibookjpa/controller/AdministradorController.java
+++ 02_BackEnd/unibook-jpa/src/main/java/com/sptech/unibookjpa/controller/AdministradorController.java
@@ -15,6 +15,8 @@ import javax.validation.Valid;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -116,6 +118,43 @@ public class AdministradorController {
         }
     }
 
+    public static void gravaArquivoTxt(List<Administrador> lista, String nomeArq) {
+        int contaRegCorpo = 0;
+
+        // Monta o registro de header
+        String header = "00CompraeVenda2022-02";
+        header += LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        header += "V1";
+
+        // Grava o registro de header
+        gravaRegistro(header, nomeArq);
+
+        // Monta e grava os registros de corpo
+        String corpo;
+        for (Administrador a : lista) {
+            corpo = "02";
+            corpo += String.format("%-5.5s", a.getId());
+            corpo += String.format("%-8.8s", a.getDataNascimento());
+            corpo += String.format("%-50.50s", a.getNome());
+            corpo += String.format("%14.14f", a.getCpf());
+            corpo += String.format("%-100.100s", a.getEmail());
+            corpo += String.format("%-100.100s", a.getSenha());
+            corpo += String.format("%-150.150s", a.getEndereco().getBairro());
+            corpo += String.format("%-100.100s", a.getEndereco().getCidade());
+            corpo += String.format("%-100.100s", a.getEndereco().getEstado());
+            corpo += String.format("%-100.100s", a.getEndereco().getLogradouro());
+            contaRegCorpo++;
+            gravaRegistro(corpo, nomeArq);
+        }
+
+        // Monta e grava o registro de trailer
+        String trailer = "01";
+        trailer += String.format("%07d", contaRegCorpo);
+        gravaRegistro(trailer, nomeArq);
+    }
+
+    public static void main(String[] args) {
 
 
+    }
 }
