915cf05e44be653b4d8ae222e79bafc1db70dceb
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-06-01 04:35:47 -0300

    Adicionando informações do formulário de Cadastro no sessionSession e criando validação

--- 03_Site/02_Site em React/unibook/src/components/CadastroEndereco.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CadastroEndereco.jsx
@@ -1,81 +1,118 @@
 import "../templates/styles/styles-cadastro.css";
 
 function CadastroEndereco() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    sessionStorage.setItem("cidade", data.cidade);
+    sessionStorage.setItem("estado", data.estado);
+    sessionStorage.setItem("logradouro", data.logradouro);
+    sessionStorage.setItem("numero", data.numero);
+    sessionStorage.setItem("cep", data.cep);
+
+    if (data.complemento) {
+      sessionStorage.setItem("complemento", data.complemento);
+    } else {
+      sessionStorage.setItem("complemento", "");
+    }
+
+    window.location.href = "/cadastro/imagem";
+  };
+
+  const backWindow = () => {
+    window.history.back();
+  };
+
   return (
-    <div className="col-12 d-flex flex-wrap align-items-center justify-content-center container-cadastro py-4 px-lg-0">
-      <div className="d-none d-lg-block col-lg-1 text-center py-4 px-lg-0">
-        <a href="/cadastro" className="text-decoration-none text-dark">
-          <button
-            style={{ width: "50px", height: "50px" }}
-            className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
-          >
-            <i className="bi bi-caret-left-fill"></i>
-          </button>
-          <div className="col-12">
-            <span className="fw-bold"> Voltar </span>
-          </div>
-        </a>
-      </div>
-      <div className="col-11 col-lg-10 bg-blue p-4 div-cadastro rounded d-flex flex-wrap align-items-center justify-content-center">
-        <h1 className="col-12 text-center text-white">Cadastro - Endereço</h1>
-        <div className="col-12 col-md-6 px-4">
-          <div className="col-12 text-white py-4">
-            <span>Cidade</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>Estado</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>Logradouro</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-        </div>
-        <div className="col-12 col-md-6 px-4">
-          <div className="col-12 text-white py-4">
-            <span>Numero</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>CEP</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>Complemento</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-        </div>
-      </div>
-      <div className="d-none d-lg-block col-lg-1 text-center py-4 px-lg-0">
-        <a href="/cadastro/imagem" className="text-decoration-none text-dark">
-          <button
-            style={{ width: "50px", height: "50px" }}
-            className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
-          >
-            <i className="bi bi-caret-right-fill"></i>
-          </button>
-          <div className="col-12">
-            <span className="fw-bold"> Proximo </span>
-          </div>
-        </a>
-      </div>
-      <div className="col-12 d-flex d-lg-none">
-        <div className="text-center py-4 px-lg-0 col-6">
-          <a href="/cadastro" className="text-decoration-none text-dark">
+    <form onSubmit={handleSubmit}>
+      <div className="col-12 d-flex flex-wrap align-items-center justify-content-center container-cadastro py-4 px-lg-0">
+        <div className="d-none d-lg-block col-lg-1 text-center py-4 px-lg-0">
+          <span className="text-decoration-none text-dark">
             <button
               style={{ width: "50px", height: "50px" }}
               className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
+              onClick={backWindow}
             >
               <i className="bi bi-caret-left-fill"></i>
             </button>
             <div className="col-12">
-              <span className="fw-bold"> Voltar </span>
+              <span className="fw-bold" onClick={backWindow}>
+                {" "}
+                Voltar{" "}
+              </span>
             </div>
-          </a>
+          </span>
         </div>
-        <div className="text-center py-4 px-lg-0 col-6">
-          <a href="/cadastro/imagem" className="text-decoration-none text-dark">
+        <div className="col-11 col-lg-10 bg-blue p-4 div-cadastro rounded d-flex flex-wrap align-items-center justify-content-center">
+          <h1 className="col-12 text-center text-white">Cadastro - Endereço</h1>
+          <div className="col-12 col-md-6 px-4">
+            <div className="col-12 text-white py-4">
+              <span>Cidade</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                name="cidade"
+                type="text"
+                placeholder="Digite sua cidade ..."
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>Estado</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                name="estado"
+                type="text"
+                placeholder="Digite seu estado ..."
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>Logradouro</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                name="logradouro"
+                type="text"
+                placeholder="Digite seu logradouro ..."
+                required
+              />
+            </div>
+          </div>
+          <div className="col-12 col-md-6 px-4">
+            <div className="col-12 text-white py-4">
+              <span>Numero</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                name="numero"
+                type="text"
+                placeholder="Digite o numero da sua casa ..."
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>CEP</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                name="cep"
+                type="text"
+                placeholder="Digite seu CEP ..."
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>Complemento (Opcional)</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                name="complemento"
+                type="type"
+                placeholder="Digite o complemento da sua casa ..."
+              />
+            </div>
+          </div>
+        </div>
+        <div className="d-none d-lg-block col-lg-1 text-center py-4 px-lg-0">
+          <span type="submit" className="text-decoration-none text-dark">
             <button
               style={{ width: "50px", height: "50px" }}
               className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
@@ -85,10 +122,42 @@ function CadastroEndereco() {
             <div className="col-12">
               <span className="fw-bold"> Proximo </span>
             </div>
-          </a>
+          </span>
+        </div>
+        <div className="col-12 d-flex d-lg-none">
+          <div className="text-center py-4 px-lg-0 col-6">
+            <span className="text-decoration-none text-dark">
+              <button
+                style={{ width: "50px", height: "50px" }}
+                className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
+                onClick={backWindow}
+              >
+                <i className="bi bi-caret-left-fill"></i>
+              </button>
+              <div className="col-12">
+                <span className="fw-bold" onClick={backWindow}>
+                  {" "}
+                  Voltar{" "}
+                </span>
+              </div>
+            </span>
+          </div>
+          <div className="text-center py-4 px-lg-0 col-6">
+            <span type="submit" className="text-decoration-none text-dark">
+              <button
+                style={{ width: "50px", height: "50px" }}
+                className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
+              >
+                <i className="bi bi-caret-right-fill"></i>
+              </button>
+              <div className="col-12">
+                <span className="fw-bold"> Proximo </span>
+              </div>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 export default CadastroEndereco;
--- 03_Site/02_Site em React/unibook/src/components/CadastroUsuario.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CadastroUsuario.jsx
@@ -1,56 +1,134 @@
 import "../templates/styles/styles-cadastro.css";
 
 function CadastroUsuario() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    sessionStorage.setItem("cpf", data.cpf);
+    sessionStorage.setItem("usuario", data.usuario);
+    sessionStorage.setItem("email", data.email);
+    sessionStorage.setItem("senha", data.senha);
+    sessionStorage.setItem("data", data.data);
+    sessionStorage.setItem("sexo", data.sexo);
+
+    window.location.href = "/cadastro/endereco";
+  };
+
   return (
-    <div className="col-12 d-flex flex-wrap align-items-center justify-content-center container-cadastro py-4 px-lg-0">
-      <div className="d-none d-lg-block col-lg-1 text-center">
-        <div style={{ width: "50px", height: "50px" }}></div>
-      </div>
-      <div className="col-11 col-lg-10 bg-blue p-4 div-cadastro rounded d-flex flex-wrap align-items-center justify-content-center">
-        <h1 className="col-12 text-center text-white">Cadastro - Usuario</h1>
-        <div className="col-12 col-md-6 px-4">
-          <div className="col-12 text-white py-4">
-            <span>Usuario</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
+    <form onSubmit={handleSubmit}>
+      <div className="col-12 d-flex flex-wrap align-items-center justify-content-center container-cadastro py-4 px-lg-0">
+        <div className="d-none d-lg-block col-lg-1 text-center">
+          <div style={{ width: "50px", height: "50px" }}></div>
+        </div>
+        <div className="col-11 col-lg-10 bg-blue p-4 div-cadastro rounded d-flex flex-wrap align-items-center justify-content-center">
+          <h1 className="col-12 text-center text-white">Cadastro - Usuario</h1>
+          <div className="col-12 col-md-6 px-4">
+            <div className="col-12 text-white py-4">
+              <span>Usuario</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                type="text"
+                placeholder="digite seu usuario ..."
+                name="usuario"
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>Email</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                type="text"
+                placeholder="digite seu email ..."
+                name="email"
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>Senha</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                type="password"
+                placeholder="digite uma senha ..."
+                name="senha"
+                required
+              />
+            </div>
           </div>
-          <div className="col-12 text-white py-4">
-            <span>Email</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>Senha</span>
-            <input className="col-12 border border-0 rounded-pill px-3" type="password" />
+          <div className="col-12 col-md-6 px-4">
+            <div className="col-12 text-white py-4">
+              <span>Data Nascimento</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                type="date"
+                name="data"
+                required
+              />
+            </div>
+            <div className="col-12 text-white py-4">
+              <span className="col-12">Sexo</span>
+              <div className="col-12 d-flex flex-wrap">
+                <div className="col-4">
+                  <input
+                    type="radio"
+                    placeholder="digite seu genero ..."
+                    name="sexo"
+                    value="F"
+                    required
+                  />{" "}
+                  Feminino
+                </div>
+                <div className="col-4">
+                  <input
+                    type="radio"
+                    placeholder="digite seu genero ..."
+                    name="sexo"
+                    value="M"
+                    required
+                  />{" "}
+                  Masculino
+                </div>
+                <div className="col-4">
+                  <input
+                    type="radio"
+                    placeholder="digite seu genero ..."
+                    name="sexo"
+                    value="O"
+                    required
+                  />{" "}
+                  Outros
+                </div>
+              </div>
+            </div>
+            <div className="col-12 text-white py-4">
+              <span>CPF</span>
+              <input
+                className="col-12 border border-0 rounded-pill px-3"
+                type="cpf"
+                maxLength={12}
+                placeholder="digite seu CPF ..."
+                name="cpf"
+                required
+              />
+            </div>
           </div>
         </div>
-        <div className="col-12 col-md-6 px-4">
-          <div className="col-12 text-white py-4">
-            <span>Data Nascimento</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>Sexo</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
-          <div className="col-12 text-white py-4">
-            <span>CPF</span>
-            <input className="col-12 border border-0 rounded-pill px-3" />
-          </div>
+        <div className="col-12 col-lg-1 text-center py-4 px-lg-0">
+          <span className="text-decoration-none text-dark" type="submit">
+            <button
+              style={{ width: "50px", height: "50px" }}
+              className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
+            >
+              <i className="bi bi-caret-right-fill"></i>
+            </button>
+            <div className="col-12">
+              <span className="fw-bold"> Proximo </span>
+            </div>
+          </span>
         </div>
       </div>
-      <div className="col-12 col-lg-1 text-center py-4 px-lg-0">
-        <a href="/cadastro/endereco" className="text-decoration-none text-dark">
-          <button
-            style={{ width: "50px", height: "50px" }}
-            className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
-          >
-            <i className="bi bi-caret-right-fill"></i>
-          </button>
-          <div className="col-12">
-            <span className="fw-bold"> Proximo </span>
-          </div>
-        </a>
-      </div>
-    </div>
+    </form>
   );
 }
 
