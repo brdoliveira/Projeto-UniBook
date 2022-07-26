326134293387f3bbad6515b6ee5d3591a8bc3e8a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-07-26 20:16:59 -0300

    FEAT(CadastroImagem): Alterando estutura do componente Cadastro Imagem

--- 03_Site/02_Site em React/unibook/src/components/CadastroImagem.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CadastroImagem.jsx
@@ -1,129 +1,40 @@
+import React from "react";
 import iconAddImage from "../templates/images/icon-add-image.png";
-import "../templates/styles/styles-cadastro.css";
 
-function CadastroImagem() {
-  /*
-  let photo = document.getElementById("imgPhoto");
-  let file = document.getElementById("flImage");
-
-  const handleInputChange = (e) => {
-    console.log(e)
-    if (e.type != 'change') {
-      file.click();
-    } else {
-      if (file.files.length <= 0) {
-        return;
-      }
-
-      let reader = new FileReader();
-
-      reader.onload = () => {
-        photo.src = reader.result;
-      };
-
-      reader.readAsDataURL(file.files[0]);
-    }
-  };
-  */
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-    // Está retornando um Objeto Imagem tratar para pegar o "hash" da imagem   
-    sessionStorage.setItem("img", data.img)
-    alert("Cadastro Efetuado ! ")
-    window.location.href = "/";
-
-  };
-
-  const backWindow = () => {
-    window.history.back();
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="col-12 d-flex flex-wrap align-items-center justify-content-center container-cadastro py-4 px-lg-0">
-          <div className="col-1 text-center d-none d-lg-block">
-            <span className="text-decoration-none text-dark">
-              <button
-                style={{ width: "50px", height: "50px" }}
-                className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
-                onClick={backWindow}
-              >
-                <i className="bi bi-caret-left-fill"></i>
-              </button>
-              <div className="col-12">
-                <span className="fw-bold" onClick={backWindow}>
-                  {" "}
-                  Voltar{" "}
-                </span>
-              </div>
-            </span>
-          </div>
-          <div className="col-11 col-lg-10 bg-blue p-4 div-cadastro rounded d-flex flex-wrap align-items-center justify-content-center">
-            <h1 className="col-12 text-center text-white">Cadastro - Imagem</h1>
-            <div className="col-12 d-flex flex-wrap align-items-center justify-content-center">
-              <div
-                className="col-12 bg-white rounded-pill d-flex flex-wrap align-items-center justify-content-center"
+class CadastroImagem extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="col-12 bg-blue rounded d-flex flex-wrap align-items-center justify-content-center">
+          <h1 className="col-12 text-center text-white">Cadastro - Imagem</h1>
+          <div className="col-12 d-flex flex-wrap align-items-center justify-content-center">
+            <div
+              className="col-12 bg-white rounded-pill d-flex flex-wrap align-items-center justify-content-center"
+              style={{ height: "300px", width: "300px" }}
+            >
+              <img
+                src={iconAddImage}
+                alt=""
+                id="imgPhoto"
+                className="rounded-pill"
                 style={{ height: "300px", width: "300px" }}
-              >
-                <img
-                  src={iconAddImage}
-                  alt=""
-                  id="imgPhoto"
-                  className="rounded-pill"
-                  style={{ height: "300px", width: "300px" }}
-                />
-              </div>
-              <div className="col-12 text-center py-2">
-                <input
-                  type="file"
-                  name="img"
-                  id="flImage"
-                  className="btn btn-success bg-orange rounded-pill border border-dark border-2 fw-bold px-4 py-1 button-cadastro"
-                  accept="image/png, image/jpeg"
-                  required
-                />
-              </div>
+              />
+            </div>
+            <div className="col-12 text-center py-2">
+              <input
+                type="file"
+                name="img"
+                id="flImage"
+                className="btn btn-success bg-orange rounded-pill border border-dark border-2 fw-bold px-4 py-1 button-cadastro"
+                accept="image/png, image/jpeg"
+                required
+              />
             </div>
           </div>
-          <div className="col-1 text-center d-none d-lg-block">
-            <div style={{ width: "50px", height: "50px" }}></div>
-          </div>
         </div>
-        <div className="col-11 text-end d-none d-lg-block">
-          <button className="bg-orange rounded-pill border border-dark border-2 fw-bold px-4 py-1 button-cadastro">
-            Finalizar Cadastro
-          </button>
-        </div>
-        <div className="col-11 d-flex d-block d-lg-none">
-          <div className="col-6 text-center">
-            <span className="text-decoration-none text-dark">
-              <button
-                style={{ width: "50px", height: "50px" }}
-                className="border border-dark border-2 rounded-pill bg-orange button-cadastro"
-                onClick={backWindow}
-              >
-                <i className="bi bi-caret-left-fill"></i>
-              </button>
-              <div className="col-12">
-                <span className="fw-bold" onClick={backWindow}>
-                  {" "}
-                  Voltar{" "}
-                </span>
-              </div>
-            </span>
-          </div>
-          <div className="col-6 text-center">
-            <button className="bg-orange rounded-pill border border-dark border-2 fw-bold px-4 py-1 button-cadastro" type="submit">
-              Finalizar Cadastro
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+      </>
+    );
+  }
 }
 
 export default CadastroImagem;
