09671277152342ed9bf53ef61238502a78c84146
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-07-27 23:51:48 -0300

    FEAT(App.js): Importando primefaces e prime icons

--- 03_Site/02_Site em React/unibook/src/App.js
+++ 03_Site/02_Site em React/unibook/src/App.js
@@ -1,6 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";      // core css
+import "bootstrap/dist/js/bootstrap.js";            // js
+import "bootstrap-icons/font/bootstrap-icons.css";  // icons
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 
 import Rotas from "./routes";
 
