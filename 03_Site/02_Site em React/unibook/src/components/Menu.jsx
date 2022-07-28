4b8cb6dd3e1c13f0136130da2b2aee926748ffe7
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-07-27 23:56:05 -0300

    FEAT(Menu): Alterando style do componente de menu

--- 03_Site/02_Site em React/unibook/src/components/Menu.jsx
+++ 03_Site/02_Site em React/unibook/src/components/Menu.jsx
@@ -19,7 +19,7 @@ function Menu() {
               />
               <div className="input-group-append">
                 <button
-                  className="btn btn-search text-white search-button border border-0 py-2"
+                  className="btn btn-search text-white search-button border border-0 py-2 button-menu"
                   type="submit"
                 >
                   <i className="bi bi-search"></i>
@@ -30,7 +30,7 @@ function Menu() {
         </div>
         <div className="col-2 col-sm-2 d-bloc d-lg-none">
           <button
-            className="navbar-toggler mx-2 mt-y border border-1 border-white me-4"
+            className="navbar-toggler mx-2 mt-y border border-1 border-white me-4 button-menu"
             style={{ width: "40px", height: "40px" }}
             type="button"
             data-bs-toggle="collapse"
@@ -47,23 +47,23 @@ function Menu() {
           id="navbarSupportedContent"
         >
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-auto">
-            <li className="nav-item rounded mx-1">
+            <li className="nav-item rounded mx-1 li-menu">
               <a
-                className="nav-link text-white px-3"
+                className="nav-link text-white px-3 a-menu"
                 aria-current="page"
                 href="/fale-conosco"
               >
                 Fale Conosco
               </a>
             </li>
-            <li className="nav-item rounded mx-1">
+            <li className="nav-item rounded mx-1 li-menu">
               <a className="nav-link text-white px-3" href="/quem-somos">
                 Quem Somos
               </a>
             </li>
-            <li className="nav-item dropdown">
+            <li className="nav-item dropdown li-menu">
               <button
-                className="text-white nav-link dropdown-toggle btn btn-secondary dropdown-toggle bg-blue border border-white border-1 p-2 d-lg-block d-none"
+                className="text-white nav-link dropdown-toggle btn btn-secondary dropdown-toggle bg-blue border border-white border-1 p-2 d-lg-block d-none button-menu"
                 id="navbarDropdown"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
@@ -74,17 +74,17 @@ function Menu() {
                 className="w-25 dropdown-menu bg-blue border border-white border-2 p-2"
                 aria-labelledby="navbarDropdown"
               >
-                <li>
+                <li className="li-menu">
                   <a
-                    className="dropdown-item rounded border border-white border-3 px-2 fw-bold text-center text-white"
+                    className="dropdown-item rounded border border-white border-3 px-2 fw-bold text-center text-white a-menu"
                     href="/login"
                   >
                     Login
                   </a>
                 </li>
-                <li className="rounded">
+                <li className="rounded li-menu">
                   <a
-                    className="dropdown-item mt-2 rounded text-white rounded"
+                    className="dropdown-item mt-2 rounded text-white rounded a-menu"
                     href="/cadastro"
                   >
                     Cliente novo?
@@ -96,14 +96,14 @@ function Menu() {
             </li>
             <div className="col-12 d-block d-lg-none py-2">
               <div className="col-12 bg-blue rounded border border-white border-2 p-2 text-center">
-                <a className="text-decoration-none" href="/login">
-                  <li className="col-12 rounded border border-white border-3 px-2 fw-bold text-center text-white py-2">
+                <a className="text-decoration-none a-menu" href="/login">
+                  <li className="col-12 rounded border border-white border-3 px-2 fw-bold text-center text-white py-2 li-menu">
                     Login
                   </li>
                 </a>
                 <div className="col-12">
-                  <a className="text-decoration-none" href="/cadastro">
-                    <li className="mt-2 rounded text-white">
+                  <a className="text-decoration-none a-menu" href="/cadastro">
+                    <li className="mt-2 rounded text-white li-menu">
                       Cliente novo?
                       <b>Comece aqui.</b>
                     </li>
