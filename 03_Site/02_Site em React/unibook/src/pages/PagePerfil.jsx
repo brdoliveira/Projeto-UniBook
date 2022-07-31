b942e19bd2b45753cbe51834030b1140bd4258ae
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-07-30 23:59:24 -0300

    FEAT(PagePerfil): Importando tela de analytics

--- 03_Site/02_Site em React/unibook/src/pages/PagePerfil.jsx
+++ 03_Site/02_Site em React/unibook/src/pages/PagePerfil.jsx
@@ -1,13 +1,37 @@
+import React from "react";
 import Menu from "../components/Menu";
-import Perfil from "../components/Perfil";
 
-function PagePerfil() {
-  return (
-    <>
-      <Menu />
-      <Perfil />
-    </>
-  );
+import { TabView, TabPanel } from "primereact/tabview";
+
+import PerfilUsuario from "../components/PerfilUsuario";
+import PerfilVendedor from "../components/PerfilVendedor";
+import PerfilAnalitycs from "../components/PerfilAnalytics";
+class PagePerfil extends React.Component {
+  render() {
+    return (
+      <>
+        <Menu />
+        <div
+          className="col-12 d-flex justify-content-center align-items-center"
+          style={{ height: "90vh" }}
+        >
+          <div className="col-10">
+            <TabView className="perfil-tabview">
+              <TabPanel header="Usuario">
+                <PerfilUsuario className="col-12" />
+              </TabPanel>
+              <TabPanel header="Vendedor">
+                <PerfilVendedor className="col-12" />
+              </TabPanel>
+              <TabPanel header="Analytics">
+                <PerfilAnalitycs className="col-12" />
+              </TabPanel>
+            </TabView>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default PagePerfil;
