9f9a8cb913a6b8d5d111d06cf29d8c464883fbc9
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-01 00:52:13 -0300

    FEAT(UnibookJpaApplication): Implementando WebConfiguration

--- 02_BackEnd/api-projeto-unibook-java/src/main/java/br/com/sptech/apiprojetounibookjava/UnibookJpaApplication.java
+++ 02_BackEnd/api-projeto-unibook-java/src/main/java/br/com/sptech/apiprojetounibookjava/UnibookJpaApplication.java
@@ -3,10 +3,11 @@ package br.com.sptech.apiprojetounibookjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableFeignClients
 @SpringBootApplication
-public class UnibookJpaApplication {
+public class UnibookJpaApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnibookJpaApplication.class, args);
