9956b2174a3394ec6860283489594d89a9eee154
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-03 00:31:21 -0300

    FEAT(PageCadastro): Recebendo dados do componente filho para efetuar o Login

--- 03_Site/02_Site em React/unibook/src/pages/PageCadastro.jsx
+++ 03_Site/02_Site em React/unibook/src/pages/PageCadastro.jsx
@@ -5,17 +5,26 @@ import { Button } from "primereact/button";
 import { Steps } from "primereact/steps";
 
 import CadastroUsuario from "../components/CadastroUsuario";
-import CadastroEndereco from "../components/CadastroEndereco";
+// import CadastroEndereco from "../components/CadastroEndereco";
 import CadastroImagem from "../components/CadastroImagem";
+import Menu from "../components/Menu";
+
+import CompradorService from "../app/service/compradorService";
+import { mensagemErro } from "../components/Toastr";
 
 import "../templates/styles/styles-cadastro.css";
-import Menu from "../components/Menu";
 
 class PageCadastro extends React.Component {
   state = {
     tabcard: 0,
     buttonLeft: true,
     buttonRight: false,
+    data: [],
+  };
+  
+  constructor(){
+    super()
+    this.service = new CompradorService();
   };
 
   disabled(index) {
@@ -23,15 +32,42 @@ class PageCadastro extends React.Component {
       this.setState({ buttonLeft: true });
       this.setState({ buttonRight: false });
     } else if (index === 1) {
-      this.setState({ buttonLeft: false });
-      this.setState({ buttonRight: false });
-    } else if (index === 2) {
+      //   this.setState({ buttonLeft: false });
+      //   this.setState({ buttonRight: false });
+      // } else if (index === 2) {
       this.setState({ buttonLeft: false });
       this.setState({ buttonRight: true });
     }
   }
 
-  items = [{ label: "Usuario" }, { label: "Endereço" }, { label: "Imagem" }];
+  items = [
+    { label: "Usuario" },
+    //  { label: "Endereço" },
+    { label: "Imagem" },
+  ];
+
+  handleCallback = (childData) => {
+    this.setState({ data: childData });
+  };
+
+  doRegistration = () => {
+    this.service
+    .cadastrar({
+      nome: this.state.data.email,
+      dataNascimento: this.state.data.dataNasc,
+      senha: this.state.data.senha,
+      cpf: this.state.data.cpf,
+      email: this.state.data.email,
+      sexo:this.state.data.sexo
+    })
+    .then((response) => {
+      window.location.href = "/login"
+    })
+    .catch((erro) => {
+      mensagemErro(erro.message);
+      return
+    });
+  }
 
   render() {
     const clickLeft = () => {
@@ -88,11 +124,14 @@ class PageCadastro extends React.Component {
                   className="cadastro"
                 >
                   <TabPanel header="Usuario">
-                    <CadastroUsuario className="col-12" />
+                    <CadastroUsuario
+                      parentCallback={this.handleCallback}
+                      className="col-12"
+                    />
                   </TabPanel>
-                  <TabPanel header="Endereço">
+                  {/* <TabPanel header="Endereço">
                     <CadastroEndereco className="col-12" />
-                  </TabPanel>
+                  </TabPanel> */}
                   <TabPanel header="Imagem">
                     <CadastroImagem className="col-12" />
                   </TabPanel>
@@ -102,6 +141,7 @@ class PageCadastro extends React.Component {
                     className="bg-orange rounded-pill border border-dark border-2 fw-bold px-4 py-1 text-dark"
                     label="Finalizar Cadastro"
                     hidden={!this.state.buttonRight}
+                    onClick={this.doRegistration}
                   />
                 </div>
               </div>
