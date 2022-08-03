e7d31c0772de20f494419ce9c71b3ef918a206fd
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-03 00:28:58 -0300

    FEAT(compradorService): Criando Comprador Service para implementar o cadastro do usuario

---
+++ 03_Site/02_Site em React/unibook/src/app/service/compradorService.js
@@ -0,0 +1,13 @@
+import ApiService from "../apiservice";
+
+class CompradorService extends ApiService{
+    constructor(){
+        super("/compradores")
+    }
+
+    cadastrar(novoUsuario){
+        return this.post("/cadastrar", novoUsuario)
+    }    
+}
+
+export default CompradorService;
