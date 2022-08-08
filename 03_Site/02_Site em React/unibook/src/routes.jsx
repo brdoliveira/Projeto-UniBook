25aab354782be31a86c67442d2c0c40d5b5f7d09
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-08 01:11:04 -0300

    FEAT(PageAdicionarProduto): Adicionando path do adicionar-produto

--- 03_Site/02_Site em React/unibook/src/routes.jsx
+++ 03_Site/02_Site em React/unibook/src/routes.jsx
@@ -9,6 +9,7 @@ import PageFaleConosco from "./pages/PageFaleConosco";
 import PageHome from "./pages/PageHome";
 import PageQuemSomos from "./pages/PageQuemSomos";
 import PageCadastro from "./pages/PageCadastro";
+import PageAdicionarProduto from "./pages/PageAdicionarProduto";
 
 function Rotas() {
   return (
@@ -19,7 +20,8 @@ function Rotas() {
         <Route path="/login" element={<PageLogin />} />
         <Route path="/perfil" element={<PagePerfil />} />
         <Route path="/perfil/editar" element={<PageEditarPerfil />} />
-        <Route path="/cadastro" element={<PageCadastro/>} />
+        <Route path="/perfil/adicionar-produto" element={<PageAdicionarProduto />} />
+        <Route path="/cadastro" element={<PageCadastro />} />
         <Route path="/fale-conosco" element={<PageFaleConosco />} />
         <Route path="/quem-somos" element={<PageQuemSomos />} />
       </Routes>
