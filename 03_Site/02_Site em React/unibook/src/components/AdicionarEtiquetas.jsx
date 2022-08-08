57874832956e5f8fa5eabe6390ab88895cc75ebe
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-08 01:07:02 -0300

    FEAT(AdicionarEtiquetas): Criando Componente de Adicionar Etiquetas

---
+++ 03_Site/02_Site em React/unibook/src/components/AdicionarEtiquetas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class AdicionarEtiquetas extends React.Component{
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
+
+export default AdicionarEtiquetas;
