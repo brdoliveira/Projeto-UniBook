a264afb2d4e45b663613f28c60d5ef180cdc56af
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-10 23:53:23 -0300

    FEAT(ProdutoCarrinho): Criando item para o carrinho

---
+++ 03_Site/02_Site em React/unibook/src/components/ProdutoCarrinho.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class ProdutoCarrinho extends React.Component {
+  render() {
+    return (
+      <div className="col-12 border border-3 rounded my-2 p-2 border-blue d-flex flex-wrap">
+        <div className="col-4">
+            <img src="" alt="" />
+        </div>
+        <div className="col-6">
+        <p className="fw-bold">Nome</p>
+        <p className="text-wrap">Mensagem</p>
+        </div>
+        <div className="col-2">
+            <p>Preço</p>
+            <p>00,00</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProdutoCarrinho;
