16a5eee8c6f9d599c7e63db1134da362273e453b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-13 23:52:46 -0300

    FEAT(PageMaisVendidos): Criando pagina dos produtos mais vendidos

---
+++ 03_Site/02_Site em React/unibook/src/pages/PageMaisVendidos.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Menu from "../components/Menu";
+import MaisVendidosCategorias from "../components/MaisVendidosCategorias";
+import CardProduto from "../components/CardProduto";
+
+class PageMaisVendidos extends React.Component {
+  render() {
+    return (
+      <>
+        <Menu />
+        <div className="col-12 d-flex flex-wrap">
+          <MaisVendidosCategorias />
+          <div className="col-10 d-flex flex-wrap h-100">
+            <div className="col-12 d-flex flex-wrap py-3 px-2 text-center" style={{heigth: '10 vh'}}>
+              <h1>Livros mais vistos</h1>
+            </div>
+            <div className="col-12 d-flex flex-wrap py-3 px-2">
+              <CardProduto />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default PageMaisVendidos;
