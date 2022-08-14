07dcef6fdf8afaeff5327e020d883ff6364b025a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-13 23:42:53 -0300

    FEAT(MaisVendidosCategoriass): Criando filtro dos mais vendidos

--- 03_Site/02_Site em React/unibook/src/components/FilterProduto.jsx
+++ 03_Site/02_Site em React/unibook/src/components/FilterProduto.jsx
@@ -7,67 +7,54 @@ import { Dropdown } from "primereact/dropdown";
 import { RadioButton } from 'primereact/radiobutton';
 
 import EnderecoService from "../app/service/enderecoService";
+const service = new EnderecoService();
 
-class FilterProduct extends React.Component {
-  constructor() {
-    super();
-    this.enderecoService = new EnderecoService();
-  }
-
-  state = {
-    cidade: "",
-    estado: "",
-    logradouro: "",
-    numero: "",
-    cep: "",
-    complemento: "",
-  };
-
-  render() {
-    const estados = this.enderecoService.obterListaEstados();
-
-    const [valueRating, setValueRating] = useState([0, 100]);
-
+export default function FilterProduto() {
     const categories = [{name: 'Accounting', key: 'A'}, {name: 'Marketing', key: 'M'}, {name: 'Production', key: 'P'}, {name: 'Research', key: 'R'}];
+    
+    const [valueRating, setValueRating] = useState([0, 100]);
     const [selectedCategory, setSelectedCategory] = useState(categories[1]);
+    const [selectedEstado,setSelectedEstado] = useState();
+    const estados = service.obterListaEstados();
+
 
     return (
-      <div className="col-4">
+      <div className="col-2">
         <h2>Filtrar Por</h2>
         <Divider />
         <h3>Avaliação</h3>
-        <p>
-          <Rating value={5} readOnly stars={5} cancel={false} /> 5
-        </p>
-        <p>
-          <Rating value={4} readOnly stars={5} cancel={false} /> 4
-        </p>
-        <p>
-          <Rating value={3} readOnly stars={5} cancel={false} /> 3
-        </p>
-        <p>
-          <Rating value={2} readOnly stars={5} cancel={false} /> 2
-        </p>
-        <p>
-          <Rating value={1} readOnly stars={5} cancel={false} /> 1
-        </p>
+        <span>
+          <Rating value={5} readOnly stars={5} cancel={false} />
+        </span>
+        <span>
+          <Rating value={4} readOnly stars={5} cancel={false} />
+        </span>
+        <span>
+          <Rating value={3} readOnly stars={5} cancel={false} />
+        </span>
+        <span>
+          <Rating value={2} readOnly stars={5} cancel={false} />
+        </span>
+        <span>
+          <Rating value={1} readOnly stars={5} cancel={false}/>
+        </span>
         <Divider />
         <h3>Faixa de Preço</h3>
         <div className="col-12 d-flex flex-wrap">
           <div className="col-6">
-            <p className="text-bold">Min:</p>
-            <p>{value[0]}</p>
+            <p className="text-bold">Min: {valueRating[0]}</p>
           </div>
           <div className="col-6">
-            <p className="text-bold">Máx:</p>
-            <p>{value[1]}</p>
+            <p className="text-bold">Máx: {valueRating[1]}</p>
           </div>
         </div>
+        <div className="col-12">
         <Slider
           value={valueRating}
           onChange={(e) => setValueRating(e.value)}
           range
-        />
+          />
+          </div>
         <Divider />
         <h3>Etiqueta</h3>
         {categories.map((category) => {
@@ -88,16 +75,13 @@ class FilterProduct extends React.Component {
         <Divider />
         <h3>Estado</h3>
         <Dropdown
-          value={this.state.estado}
+          value={selectedEstado}
           options={estados}
-          onChange={(e) => this.setState({ estado: e.target.value })}
+          onChange={(e) => setSelectedEstado(e.target.value)}
           className="col-12 border border-0 rounded-pill"
           optionLabel="name"
           placeholder="digite seu estado..."
         />
       </div>
     );
-  }
 }
-
-export default FilterProduct;
---
+++ 03_Site/02_Site em React/unibook/src/components/MaisVendidosCategorias.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Divider } from "primereact/divider";
+
+import ListaMaisVendidos from "../components/ListaMaisVendidos";
+
+class MaisVendidosCategorias extends React.Component {
+
+  state = {
+    opcoes : { 
+        "Materias" : ["Português","Matemática","Geográfia","História","Fisíca"],
+        "Idiomas" : ["Ingles","Espanhol"],
+        "Outros" : ["Poesia","Romance","Drama","Contos"]
+    }
+  };
+
+  render() {
+    return (
+      <div className="col-2">
+        <h2>Filtrar Por</h2>
+        <Divider />
+        <ListaMaisVendidos lista={this.state.opcoes} />
+      </div>
+    );
+  }
+}
+
+export default MaisVendidosCategorias;
