378d0156d7ca09c20f16bcd37bf20e156e4f4372
Luiz Henrique <luiz.paz@bandtec.com.br>
2022-08-15 00:16:18 -0300

    FEAT(api-pagamento-gerencianet-nodejs): Adicionando esqueleto da api de pagamento

---
+++ 02_BackEnd/api-pagamento-gerencianet-nodejs/src/server.js
@@ -0,0 +1,55 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const GNRequest = require('./apis/gerencianet');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+const reqGNAlready = GNRequest({
+  clientID: process.env.GN_CLIENT_ID,
+  clientSecret: process.env.GN_CLIENT_SECRET
+});
+
+app.get('/', async (req, res) => {
+  const reqGN = await reqGNAlready;
+  const dataCob = {
+    calendario: {
+      expiracao: 3600
+    },
+    valor: {
+      original: '0.10'
+    },
+    chave: '126bec4a-2eb6-4b79-a045-78db68412899',
+    solicitacaoPagador: 'Cobrança dos serviços prestados.'
+  };
+
+  const cobResponse = await reqGN.post('/v2/cob', dataCob);
+  const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`);
+
+  res.render('qrcode', { qrcodeImage: qrcodeResponse.data.imagemQrcode })
+});
+
+app.get('/cobrancas', async(req, res) => {
+  const reqGN = await reqGNAlready;
+
+  const cobResponse = await reqGN.get('/v2/cob?inicio=2021-02-15T16:01:35Z&fim=2021-02-22T23:59:00Z');
+
+  res.send(cobResponse.data);
+});
+
+app.post('/webhook(/pix)?', (req, res) => {
+  console.log(req.body);
+  res.send('200');
+});
+
+app.listen(8000, () => {
+  console.log('running');
+})
