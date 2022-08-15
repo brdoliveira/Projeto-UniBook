4505924b60a23445d9d77930bbf21687c135ce46
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-15 00:31:19 -0300

    FEAT(FeedbackMessage): Componente das mensagens de feedback

--- 03_Site/02_Site em React/unibook/src/components/FeedbackMessage.jsx
+++ 03_Site/02_Site em React/unibook/src/components/FeedbackMessage.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 
-class FeedbackMessage extends React.Component{
-    render(){
-        return(
-            <div className="col-12 border border-3 rounded my-2 p-2 border-blue">
-                <p className="fw-bold">Nome</p>
-                <p className="text-wrap">Mensagem</p>
-            </div>
-        )
-    }
+export default function FeedbackMessage() {
+  return (
+    <div className="col-12 border border-3 rounded my-2 p-2 border-blue">
+      <p className="fw-bold">Nome</p>
+      <p className="text-wrap">Mensagem</p>
+    </div>
+  );
 }
-
-export default FeedbackMessage;
