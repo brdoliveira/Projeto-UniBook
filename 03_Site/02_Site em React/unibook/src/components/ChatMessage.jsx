3efb27d44f6ae33f457723b8ec9066c8d2ed4651
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-14 23:59:47 -0300

    FEAT(ChatMessage): Criando componente de mensagem do chat

---
+++ 03_Site/02_Site em React/unibook/src/components/ChatMessage.jsx
@@ -0,0 +1,36 @@
+export default function ChatMessage(props) {
+  if (props.proprietarioMensagem) {
+    return (
+      <div className="col-12 d-flex justify-content-end">
+        <div className="col-8 border border-3 rounded my-2 p-3 border-orange">
+          <div className="col-12 d-flex justify-content-end py-1 fw-bold">
+            00/00/0000 00:00 00
+          </div>
+          <div className="col-12 d-flex">
+            <p className="col-11 text-break px-2">
+              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="col-12 d-flex justify-content-start">
+      <div className="col-8 border border-3 rounded my-2 p-3 border-blue">
+        <div className="col-12 d-flex justify-content-end py-1 fw-bold">
+          00/00/0000 00:00 00
+        </div>
+        <div className="col-12 d-flex">
+          <p className="col-11 text-break px-2">
+            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
