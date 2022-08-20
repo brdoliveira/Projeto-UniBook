681241a3217d1417f81d8942c7a8350fc4869e3c
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-19 23:59:24 -0300

    FEAT(PageProdutoPagamento): Alinhando tela de produto pagamento

--- 03_Site/02_Site em React/unibook/src/pages/PageProdutoPagamento.jsx
+++ 03_Site/02_Site em React/unibook/src/pages/PageProdutoPagamento.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-import Menu from "../components/Menu"; 
+import Menu from "../components/Menu";
 import ProdutoPagamento from "../components/ProdutoPagamento";
 
-class PageProdutoPagamento extends React.Component{
-    render(){
-        return(
-            <>
-            <Menu/>
-            <ProdutoPagamento/>
-            </>
-        )
-    }
+import "../templates/styles/styles-pagamento.css";
+
+class PageProdutoPagamento extends React.Component {
+  render() {
+    return (
+      <>
+        <Menu />
+        <div className="col-12 d-flex flex-wrap h-100" style={{ height: "90vh" }}>
+          <ProdutoPagamento />
+        </div>
+      </>
+    );
+  }
 }
 
 export default PageProdutoPagamento;
