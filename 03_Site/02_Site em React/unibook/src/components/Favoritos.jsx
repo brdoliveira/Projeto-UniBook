cff518d416dbe0941d2434fbf7f08bd5b8d6dd07
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-21 23:48:02 -0300

    FEAT(Favorito): Melhorando estrutura do favoritos

--- 03_Site/02_Site em React/unibook/src/components/Favoritos.jsx
+++ 03_Site/02_Site em React/unibook/src/components/Favoritos.jsx
@@ -1,20 +1,52 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 import { Sidebar } from "primereact/sidebar";
+import { Button } from "primereact/button";
 
-function Favoritos(props){
-    const [visibleFavoritos, setVisibleFavoritos] = useState(props.showFavoritos);
+import ProdutoFavorito from "./ProdutoFavorito";
+import ModalExclusao from "./ModalExclusao";
 
-    return (
-      <Sidebar
+function Favoritos(props) {
+  const [visibleFavoritos, setVisibleFavoritos] = useState(props.showFavoritos);
+
+  useEffect(() => {
+    setVisibleFavoritos(props.showFavoritos);
+  }, [props.showFavoritos]);
+  
+  function onTriggerFavoritos() {
+    props.parentCallbackFavoritos(false);
+  }
+
+  return (
+    <Sidebar
       visible={visibleFavoritos}
       position="left"
       className="p-sidebar-lg bg-blue"
-      onHide={() => setVisibleFavoritos(props.showFavoritos)}
-      >
-        <h1 className="fw-bold text-white">Favoritos</h1>
-      </Sidebar>
-    );
+      onHide={() => {
+        setVisibleFavoritos(false);
+        onTriggerFavoritos();
+      }}
+    >
+      <div className="col-12 d-flex flex-wrap p-sidebar-top">
+        <div className="col-10 d-flex align-items-center">
+          <span className="fw-bold text-white display-5">Favoritos</span>
+        </div>
+        <div className="col-2 d-flex justify-content-end align-items-center">
+          <ModalExclusao header="Confirmação exclusão" message="Tem certeza que deseja limpar a lista de favoritos ?" />
+        </div>
+      </div>
+      <div className="p-sidebar-content" style={{ height: "80vh" }}>
+        <ProdutoFavorito/>
+      </div>
+      <div className="p-sidebar-bottom d-flex justify-content-end">
+        <Button
+          className="p-button-rounded bg-orange border border-3 border-dark text-dark"
+          aria-label="Carrinho"
+          label="Adicionar ao Carrinho"
+        />
+      </div>
+    </Sidebar>
+  );
 }
 
 export default Favoritos;
