59dc5516f0ae270f4965c9eb400e5de50bb608e6
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-22 00:28:10 -0300

    REFACT(ProdutoFavorito): Melhorando estrutura do ProdutoFavorito

--- 03_Site/02_Site em React/unibook/src/components/ProdutoFavorito.jsx
+++ 03_Site/02_Site em React/unibook/src/components/ProdutoFavorito.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-class ProdutoFavorito extends React.Component {
-  render() {
+export default function ProdutoFavorito(){
     return (
-      <div className="col-12 border border-3 rounded my-2 p-2 border-blue d-flex flex-wrap">
-        <div className="col-4">
-            <img src="" alt="" />
+      <div className="col-12 border border-3 rounded my-2 p-2 border-orange d-flex flex-wrap bg-orangelightbook text-dark">
+      <div className="col-4">
+        <img src="" alt="" />
+      </div>
+      <div className="col-8 d-flex flex-wrap">
+        <div className="col-11">
+          <span className="fw-bold text-wrap">Nome Sobrenomes</span>
         </div>
-        <div className="col-6">
-        <p className="fw-bold">Nome</p>
-        <p className="text-wrap">Mensagem</p>
+        <div className="col-1 d-flex align-items-end">
+          <i className="pi pi-heart-fill cursor-pointer"></i>
         </div>
-        <div className="col-2">
-            <p>Preço</p>
-            <p>00,00</p>
+        <div className="col-12">
+          <span className="text-wrap">R$ 00,00</span>
         </div>
       </div>
+    </div>
     );
-  }
 }
-
-export default ProdutoFavorito;
