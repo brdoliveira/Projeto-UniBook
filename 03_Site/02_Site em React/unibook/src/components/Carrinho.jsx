fd319d1384c391ddb34b45e3b99de466d7b6347d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-21 23:47:40 -0300

    FEAT(Carrinho): Melhorando estrutura do carrinho

--- 03_Site/02_Site em React/unibook/src/components/Carrinho.jsx
+++ 03_Site/02_Site em React/unibook/src/components/Carrinho.jsx
@@ -1,20 +1,53 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 import { Sidebar } from "primereact/sidebar";
+import { Button } from "primereact/button";
 
-function Carrinho(props){
-    const [visibleCarrinho, setVisibleCarrinho] = useState(props.showFavoritos);
+import ProdutoCarrinho  from "./ProdutoCarrinho";
+import ModalExclusao from "./ModalExclusao";
 
-    return (
-        <Sidebar
-          visible={visibleCarrinho}
-          position="left"
-          className="p-sidebar-lg bg-blue"
-          onHide={() => setVisibleCarrinho(props.showCarrinho)}
-        >
-          <h1 className="fw-bold text-white">Carrinho</h1>
-        </Sidebar>
-    );
+function Carrinho(props) {
+  const [visibleCarrinho, setVisibleCarrinho] = useState(false);
+
+  useEffect(() => {
+    setVisibleCarrinho(props.showCarrinho);
+  }, [props.showCarrinho]);
+
+  function onTriggerCarrinho() {
+    props.parentCallbackCarrinho(false);
+  }
+
+  return (
+    <Sidebar
+      visible={visibleCarrinho}
+      position="left"
+      className="p-sidebar-lg bg-blue"
+      onHide={() => {
+        setVisibleCarrinho(false);
+        onTriggerCarrinho();
+      }}
+    >
+      <div className="col-12 d-flex flex-wrap p-sidebar-top">
+        <div className="col-10 d-flex align-items-center">
+          <span className="fw-bold text-white display-5">Carrinho</span>
+        </div>
+        <div className="col-2 d-flex justify-content-end align-items-center">
+          <ModalExclusao header="Confirmação exclusão" message="Tem certeza que deseja limpar a lista de carrinho ?" />
+        </div>
+      </div>
+      <div className="p-sidebar-content" style={{ height: "80vh" }}>
+        <ProdutoCarrinho/>
+      </div>
+      <div className="p-sidebar-bottom d-flex justify-content-end align-items-center">
+        <span className="text-white fw-bold px-2"> R$ 00,00</span>
+        <Button
+          className="p-button-rounded bg-orange border border-3 border-dark text-dark"
+          aria-label="Comprar"
+          label="Comprar"
+        />
+      </div>
+    </Sidebar>
+  );
 }
 
 export default Carrinho;
