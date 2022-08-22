f440ffb40ac71f0fc52888b802da4e2ddd441f83
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-22 00:27:44 -0300

    REFACT(ProdutoCarrinho): Melhorando estrutura do ProdutoCarrinho

--- 03_Site/02_Site em React/unibook/src/components/ProdutoCarrinho.jsx
+++ 03_Site/02_Site em React/unibook/src/components/ProdutoCarrinho.jsx
@@ -1,23 +1,20 @@
-import React from "react";
-
-class ProdutoCarrinho extends React.Component {
-  render() {
-    return (
-      <div className="col-12 border border-3 rounded my-2 p-2 border-blue d-flex flex-wrap">
-        <div className="col-4">
-            <img src="" alt="" />
+export default function ProdutoCarrinho() {
+  return (
+    <div className="col-12 border border-3 rounded my-2 p-2 border-orange d-flex flex-wrap bg-orangelightbook text-dark">
+      <div className="col-4">
+        <img src="" alt="" />
+      </div>
+      <div className="col-8 d-flex flex-wrap">
+        <div className="col-11">
+          <span className="fw-bold text-wrap">Nome Sobrenome</span>
         </div>
-        <div className="col-6">
-        <p className="fw-bold">Nome</p>
-        <p className="text-wrap">Mensagem</p>
+        <div className="col-1 d-flex align-items-end">
+          <i className="pi pi-times cursor-pointer"></i>
         </div>
-        <div className="col-2">
-            <p>Preço</p>
-            <p>00,00</p>
+        <div className="col-12">
+          <span className="text-wrap">R$ 00,00</span>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default ProdutoCarrinho;
