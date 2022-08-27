55a1815e8f3cb0a22c0311b7b8ca9fd61d1549d4
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-26 21:43:51 -0300

    FEAT(PagamentoBoleto): Alinhando tela de pagamento boleto

--- 03_Site/02_Site em React/unibook/src/components/PagamentoBoleto.jsx
+++ 03_Site/02_Site em React/unibook/src/components/PagamentoBoleto.jsx
@@ -3,18 +3,18 @@ import { Button } from "primereact/button";
 export default function PagamentoBoleto() {
   return (
     <div className="col-12 d-flex flex-wrap justiy-content-center align-items-center">
-      <div className="col-10 d-flex flex-wrap">
-        <div className="col-12 fw-bold text-white">Boleto</div>
-        <div className="col-12">
+        <div className="col-12 text-center">
+          <h1 className="fw-bold text-white py-4">Boleto</h1>
+        </div>
+        <div className="col-12 text-center text-white">
           <h2>O código de acompanhamento do seu pedido é 30940499</h2>
-          <div className="col-12 d-flex align-items-center">
-            <Button label="Gerar Boleto" icon="pi pi-qrcode" iconPos="right" />
+          <div className="col-12 d-flex justify-content-center py-4">
+            <Button className="p-4 px-5" label="Gerar Boleto" icon="pi pi-qrcode" iconPos="right" />
           </div>
           <h2>
             Em instantes você receberá um e-mail de confirmação da sua compra.
           </h2>
         </div>
-      </div>
     </div>
   );
 }
