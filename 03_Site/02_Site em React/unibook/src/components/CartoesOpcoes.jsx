dc1a2ab6c0fbb53778557b2b2a62541a4c6fa543
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-26 21:31:44 -0300

    FEAT(CartoesOpcoes): Alinhando tela da lista de cartão de crédito e de debito

--- 03_Site/02_Site em React/unibook/src/components/CartoesOpcoes.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CartoesOpcoes.jsx
@@ -5,7 +5,7 @@ export default function CartoesOpcoes(props) {
   var labelsCartao = {
     title: "",
     subtitle: "",
-    selectCard : []
+    selectCard: [],
   };
 
   if (props.isCredit) {
@@ -18,19 +18,18 @@ export default function CartoesOpcoes(props) {
 
   return (
     <div className="col-12 d-flex flex-wrap justiy-content-center align-items-center">
-      <div className="col-10 d-flex flex-wrap">
-        <div className="col-12 text-center fw-bold text-white">
-          <h1>{labelsCartao.title}</h1>
-        </div>
-        <div className="col-12 text-white">
-          <h1>{labelsCartao.subtitle}</h1>
-        </div>
-        <ScrollPanel
-          className="col-12 scroll text-dark mb-3 ps-1 justify-content-center flex-wrap rounded"
-          style={{ height: "50vh" }}
-        ></ScrollPanel>
+      <div className="col-12 text-center fw-bold text-white">
+        <h1 className="fw-bold py-4">{labelsCartao.title}</h1>
       </div>
-      <div className="col-12">
+      <div className="col-12 text-white">
+        <h2>{labelsCartao.subtitle}</h2>
+      </div>
+      <div className="col-12 d-flex align-items-center">
+        <div className="col-11 d-flex">
+          <ScrollPanel className="col-12 scroll text-dark mb-3 ps-1 justify-content-center flex-wrap rounded h-auto"></ScrollPanel>
+        </div>
+      </div>
+      <div className="col-12 d-flex flex-wrap px-2">
         <div className="col-6 d-flex justify-content-start">
           <Button
             label="Adicionar cartão de crédito"
