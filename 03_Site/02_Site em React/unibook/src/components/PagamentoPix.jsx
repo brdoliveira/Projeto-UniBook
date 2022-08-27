46dcd9da6c6bf8ac41b46cc2353e6372174c89c0
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-26 21:44:33 -0300

    FEAT(PagamentoPix): Alinhando tela do pagamento por pix

--- 03_Site/02_Site em React/unibook/src/components/PagamentoPix.jsx
+++ 03_Site/02_Site em React/unibook/src/components/PagamentoPix.jsx
@@ -1,21 +1,24 @@
 import { Button } from "primereact/button";
 
-export default function PagamentoPix(){
-    return(
-        <div className="col-12 d-flex flex-wrap justiy-content-center align-items-center">
-            <div className="col-10 d-flex flex-wrap">
-                <div className="col-12 fw-bold text-white">Pix</div>
-                <div className="col-8">
-                    <h1 className="text-white fw-bold">Copie ou escanei o QR CODE</h1>
-                    <h2>
-                        Ao copiar o código, abra seu aplicativo cadastrado no PIX e 
-                        realizado o seu pagamento de forma rápida.
-                    </h2>
-                </div>
-                <div className="col-4 d-flex justify-content-end align items-end">
-                    <Button label="Cópiar Código"/>
-                </div>
-            </div>
+export default function PagamentoPix() {
+  return (
+    <div className="col-12 d-flex flex-wrap justiy-content-center align-items-center">
+      <div className="col-12 text-center">
+        <h1 className="fw-bold text-white py-4">Pix</h1>
+      </div>
+        <div className="col-12 px-5">
+          <h2 className="text-white fw-bold text-center">Copie ou escanei o QR CODE</h2>
+          <h3 className="text-white text-center">
+            Ao copiar o código, abra seu aplicativo cadastrado no PIX e
+            realizado o seu pagamento de forma rápida.
+          </h3>
         </div>
-    )
+      <div className="col-12 d-flex flex-wrap px-5 justify-content-center">
+        <div className="bg-white" style={{width: "300px", height: "300px"}}></div>
+        <div className="col-12 d-flex justify-content-center py-5">
+            <Button label="Cópiar Código"/>
+        </div>
+      </div>
+    </div>
+  );
 }
