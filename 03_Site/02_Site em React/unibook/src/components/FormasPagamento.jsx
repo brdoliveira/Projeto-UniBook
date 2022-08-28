756a36dca8af42dc387402cff5bec5e68fd1d2fd
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-27 23:51:56 -0300

    FEAT(FormasPagamento): Enviando forma de pagamento escolhida para o componente pai

--- 03_Site/02_Site em React/unibook/src/components/FormasPagamento.jsx
+++ 03_Site/02_Site em React/unibook/src/components/FormasPagamento.jsx
@@ -3,12 +3,23 @@ import Credito from "../templates/images/formas-pagamentos/cartao-de-credito.png
 import Debito from "../templates/images/formas-pagamentos/cartao-de-debito.png";
 import Pix from "../templates/images/formas-pagamentos/pix.png";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { RadioButton } from "primereact/radiobutton";
 
-export default function FormasPagamento() {
-  const [formaPagamento, setFormaPagamento] = useState("Credito");
+export default function FormasPagamento(props) {
+  const [formaPagamento, setFormaPagamento] = useState(
+    props.pagamentoEscolhido
+  );
+
+  useEffect(() => {
+    setFormaPagamento(props.pagamentoEscolhido);
+  }, [props.pagamentoEscolhido]);
+
+  function onTriggerPagamentoEscolhido(e) {
+    setFormaPagamento(e);
+    props.parentCallbackPagamentoEscolhido(e);
+  }
 
   return (
     <div className="col-12 d-flex flex-wrap">
@@ -20,11 +31,18 @@ export default function FormasPagamento() {
           <RadioButton
             inputId="formaPagamento2"
             name="formaPagamento"
-            value="Credito"
-            onChange={(e) => setFormaPagamento(e.value)}
-            checked={formaPagamento === "Credito"}
+            value="credito"
+            onChange={(e) => {
+              onTriggerPagamentoEscolhido(e.value);
+            }}
+            checked={formaPagamento === "credito"}
           />
-          <img src={Credito} alt="icon-credito" className="img-fluid p-2" style={{width: '100px',height: '100px'}} />
+          <img
+            src={Credito}
+            alt="icon-credito"
+            className="img-fluid p-2"
+            style={{ width: "100px", height: "100px" }}
+          />
         </div>
         <div className="col-8">
           <h1>Crédito</h1>
@@ -35,12 +53,19 @@ export default function FormasPagamento() {
           <RadioButton
             inputId="formaPagamento3"
             name="formaPagamento"
-            value="Debito"
-            onChange={(e) => setFormaPagamento(e.value)}
-            checked={formaPagamento === "Debito"}
+            value="debito"
+            onChange={(e) => {
+              onTriggerPagamentoEscolhido(e.value);
+            }}
+            checked={formaPagamento === "debito"}
           />
 
-          <img src={Debito} alt="icon-debito" className="img-fluid p-2" style={{width: '100px',height: '100px'}}/>
+          <img
+            src={Debito}
+            alt="icon-debito"
+            className="img-fluid p-2"
+            style={{ width: "100px", height: "100px" }}
+          />
         </div>
         <div className="col-8">
           <h1>Débito</h1>
@@ -51,11 +76,18 @@ export default function FormasPagamento() {
           <RadioButton
             inputId="formaPagamento1"
             name="formaPagamento"
-            value="Boleto"
-            onChange={(e) => setFormaPagamento(e.value)}
-            checked={formaPagamento === "Boleto"}
+            value="boleto"
+            onChange={(e) => {
+              onTriggerPagamentoEscolhido(e.value);
+            }}
+            checked={formaPagamento === "boleto"}
           />
-          <img src={Boleto} alt="icon-boleto" className="img-fluid p-2" style={{width: '100px',height: '100px'}} />
+          <img
+            src={Boleto}
+            alt="icon-boleto"
+            className="img-fluid p-2"
+            style={{ width: "100px", height: "100px" }}
+          />
         </div>
         <div className="col-8">
           <h1>Boleto</h1>
@@ -66,11 +98,18 @@ export default function FormasPagamento() {
           <RadioButton
             inputId="formaPagamento4"
             name="formaPagamento"
-            value="Pix"
-            onChange={(e) => setFormaPagamento(e.value)}
-            checked={formaPagamento === "Pix"}
+            value="pix"
+            onChange={(e) => {
+              onTriggerPagamentoEscolhido(e.value);
+            }}
+            checked={formaPagamento === "pix"}
           />
-          <img src={Pix} alt="icon-pix" className="img-fluid p-2" style={{width: '100px',height: '100px'}}/>
+          <img
+            src={Pix}
+            alt="icon-pix"
+            className="img-fluid p-2"
+            style={{ width: "100px", height: "100px" }}
+          />
         </div>
         <div className="col-8">
           <h1>Pix</h1>
