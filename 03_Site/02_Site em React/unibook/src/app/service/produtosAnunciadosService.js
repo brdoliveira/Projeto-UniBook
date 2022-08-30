de3ec82e320e72fa0f5a5e2d4b90c4d8c947f5cd
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-29 22:52:47 -0300

    FEAT(produtosAnunciadoService): Criando conexão com o BackEnd

--- 03_Site/02_Site em React/unibook/src/app/service/produtosAnunciadosService.js
+++ 03_Site/02_Site em React/unibook/src/app/service/produtosAnunciadosService.js
@@ -4,6 +4,39 @@ class ProdutosAnunciadosService extends ApiService {
   constructor() {
     super("/produtos-anuciados");
   }
+
+  salvarProduto(produto){
+    return this.post(`?produto=${produto}`);
+  }
+
+  listarTodos(){
+    return this.get("/listar-todos")
+  }
+
+  listarTodosPilha(){
+    return this.get("/listar-todos-pilha")
+  }
+
+  pesquisarProdutos(idProduto){
+    return this.get(`/listar-todos?idProduto=${idProduto}`)
+  }
+
+  foto(idProdutoAnuncio){
+    return this.get(`/foto?idProdutoAnuncio=${idProdutoAnuncio}`)
+  }
+
+  alterarFoto(idProdutoAnuncio,novaFoto){
+    return this.post(`/alterar-foto?idProdutoAnuncio=${idProdutoAnuncio}&novaFoto=${novaFoto}`)
+  }
+
+  listarAnunciosPorVendedor(nomeUsuario){
+    return this.get(`/listar-anuncios-por-vendedor?nomeUsuario=${nomeUsuario}`)
+  }
+
+  listarAnunciosPorTitulo(titulo){
+    return this.get(`/listar-anuncios-por-titulo?titulo=${titulo}`)
+  }
+
 }
 
 export default ProdutosAnunciadosService;
