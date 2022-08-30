a28f4f2b75a353a8e7f27bc4f77b233f03a55b1d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-29 23:39:14 -0300

    FEAT(CadastroImagem): Tornando File Uploader funcional

--- 03_Site/02_Site em React/unibook/src/components/CadastroImagem.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CadastroImagem.jsx
@@ -1,70 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+
 import iconAddImage from "../templates/images/icon-add-image.png";
 
 import { FileUpload } from "primereact/fileupload";
 
-class CadastroImagem extends React.Component {
-  state = {
-    imgPath: ''
-  }
-  
-  componentDidMount(){
-    this.setState({ imgPath: iconAddImage})
-  }
-  
-  render() {
-    const customBase64Uploader = async (event) => {
-      if(this.state.imgPath === iconAddImage){
-        var base64data;
-        // convert file to base64 encoded
-        const file = event.files[0];
-        const reader = new FileReader();
-        let blob = await fetch(file.objectURL).then((r) => r.blob()); //blob:url
-        reader.readAsDataURL(blob);
-        reader.onloadend = function () {
-          base64data = reader.result;
-        };
-        this.setState({imgPath : base64data})
-        console.log(this.state.imgPath)
+export default function CadastroImagem() {
+  const [imgPath, setImgPath] = useState(iconAddImage);
+
+  const customBase64Uploader = async (event) => {
+    if (imgPath === iconAddImage) {
+      var base64data;
+      const file = event.files[0];
+      const reader = new FileReader();
+      let blob = await fetch(file.objectURL).then((r) => r.blob());
+      reader.readAsDataURL(blob);
+      reader.onloadend = function () {
+        base64data = reader.result;
+        setImgPath(base64data);
       };
-
-      console.log(this.state.imgPath)
+      return;
     }
+  };
 
-    return (
-      <>
-        <div
-          className="col-12 bg-blue rounded d-flex flex-wrap align-items-center justify-content-center"
-          style={{ height: "50vh" }}
-        >
-          <h1 className="col-12 text-center text-white">Cadastro - Imagem</h1>
-          <div className="col-12 d-flex flex-wrap align-items-center justify-content-center">
-            <div
-              className="col-12 bg-white rounded-pill d-flex flex-wrap align-items-center justify-content-center"
+  return (
+    <>
+      <div
+        className="col-12 bg-blue rounded d-flex flex-wrap align-items-center justify-content-center"
+        style={{ height: "50vh" }}
+      >
+        <h1 className="col-12 text-center text-white">Cadastro - Imagem</h1>
+        <div className="col-12 d-flex flex-wrap align-items-center justify-content-center">
+          <div
+            className="col-12 bg-white rounded-pill d-flex flex-wrap align-items-center justify-content-center"
+            style={{ height: "300px", width: "300px" }}
+          >
+            <img
+              src={imgPath}
+              alt=""
+              id="imgPhoto"
+              className="rounded-pill"
               style={{ height: "300px", width: "300px" }}
-            >
-              <img
-                src={this.state.imgPath}
-                alt=""
-                id="imgPhoto"
-                className="rounded-pill"
-                style={{ height: "300px", width: "300px" }}
-              />
-            </div>
-            <div className="col-12 text-center py-2">
-              <FileUpload
-                mode="basic"
-                name="demo[]"
-                accept="image/*"
-                customUpload
-                uploadHandler={customBase64Uploader}
-              />
-            </div>
+            />
+          </div>
+          <div className="col-12 text-center py-2">
+            <FileUpload
+              mode="basic"
+              name="demo[]"
+              accept="image/*"
+              customUpload
+              uploadHandler={customBase64Uploader}
+              auto
+            />
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
-export default CadastroImagem;
