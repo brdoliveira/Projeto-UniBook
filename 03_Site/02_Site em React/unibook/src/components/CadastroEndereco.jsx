c8652c6e9220132c6a6a00ca4d99c77f8657a063
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-08-30 23:43:57 -0300

    FEAT(CadastroEndereco): Adicionando botão no input de CEP e alterando a ordem dos inputs

--- 03_Site/02_Site em React/unibook/src/components/CadastroEndereco.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CadastroEndereco.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { InputText } from "primereact/inputtext";
 import { InputMask } from "primereact/inputmask";
 import { Dropdown } from 'primereact/dropdown';
+import { Button } from "primereact/button";
 
 import EnderecoService from "../app/service/enderecoService";
 class CadastroEndereco extends React.Component {
@@ -26,12 +27,24 @@ class CadastroEndereco extends React.Component {
       <>
         <div
           className="col-12 bg-blue d-flex flex-wrap align-items-center justify-content-center"
-          style={{ height: "50vh" }}
+          style={{ height: "60vh" }}
         >
           <h1 className="col-12 text-center text-white py-1">
             Cadastro - Endereço
           </h1>
           <div className="col-12 col-md-6 px-4">
+          <div className="col-12 text-white py-4">
+              <p>CEP</p>
+              <div className="p-inputgroup">
+                <InputMask
+                  className="col-12 border border-0 rounded-pill rounded-end"
+                  mask="99999-999"
+                  value={this.state.cep}
+                  onChange={(e) => this.setState({ cep: e.target.value })}
+                />
+                <Button icon="pi pi-search" className="rounded-start rounded-pill"/>
+              </div>
+            </div>
             <div className="col-12 text-white py-4">
               <p>Cidade</p>
               <InputText
@@ -41,15 +54,6 @@ class CadastroEndereco extends React.Component {
                 type="text"
                 placeholder="digite sua cidade..."
               />
-            </div>
-            <div className="col-12 text-white py-4">
-              <p>Estado</p>
-              <Dropdown value={this.state.estado} 
-                options={estados} 
-                onChange={(e) => this.setState({ estado: e.target.value })}
-                className="col-12 border border-0 rounded-pill"
-                optionLabel="name"
-                placeholder="digite seu estado..." />
             </div>
             <div className="col-12 text-white py-4">
               <p>Logradouro</p>
@@ -74,13 +78,13 @@ class CadastroEndereco extends React.Component {
               />
             </div>
             <div className="col-12 text-white py-4">
-              <p>CEP</p>
-              <InputMask
+              <p>Estado</p>
+              <Dropdown value={this.state.estado} 
+                options={estados} 
+                onChange={(e) => this.setState({ estado: e.target.value })}
                 className="col-12 border border-0 rounded-pill"
-                mask="99999-999"
-                value={this.state.cep}
-                onChange={(e) => this.setState({ cep: e.target.value })}
-              ></InputMask>
+                optionLabel="name"
+                placeholder="digite seu estado..." />
             </div>
             <div className="col-12 text-white py-4">
               <p>Complemento (Opcional)</p>
