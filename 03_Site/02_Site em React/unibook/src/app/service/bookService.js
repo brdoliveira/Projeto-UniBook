5e3de54ffbff9daf27d09b9065df45f8d594523e
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-01 23:55:42 -0300

    FEAT(bookService): Criando service por pesquisar livro por isbn

--- 03_Site/02_Site em React/unibook/src/app/service/bookService.js
+++ 03_Site/02_Site em React/unibook/src/app/service/bookService.js
@@ -1,17 +1,28 @@
 class BookService {
   consultarLivro(isbn) {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&startIndex=0&all&lite&orderBy=relevance`,
-      {
-        method: "GET",
-        mode: "cors",
-      }
+      `https://www.googleapis.com/books/v1/volumes?q=isbn:${
+        isbn
+      }&startIndex=0&all&lite&orderBy=relevance`
     )
-      .then((res) => {
-        console.log(res);
+      .then(function (response) {
+        return response.json();
       })
-      .catch((err) => {
-        console.log(err);
+      .then(function (dados) {
+        var dadosLivros = dados.items[0].volumeInfo;
+        return {
+          dados: {
+            titulo: dadosLivros.title,
+            descricao: dadosLivros.description,
+            dataPublicacao: dadosLivros.publishedDate,
+            qtdadePaginas: dadosLivros.pageCount,
+            categorias: dadosLivros.categories,
+          },
+          sucesso: true,
+        };
+      })
+      .catch(function () {
+        return { dados: {}, sucesso: false };
       });
   }
 }
