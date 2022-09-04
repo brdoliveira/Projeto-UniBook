4cd550e289855b99e248d6328f2953bf2c51ea5a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-03 23:52:07 -0300

    FEAT(CadastroUsuario): Passando Informações para o componente pai

--- 03_Site/02_Site em React/unibook/src/components/CadastroUsuario.jsx
+++ 03_Site/02_Site em React/unibook/src/components/CadastroUsuario.jsx
@@ -1,157 +1,178 @@
-import React from "react";
+import React , { useState } from "react";
 
 import { Calendar } from "primereact/calendar";
 import { InputText } from "primereact/inputtext";
 import { InputMask } from "primereact/inputmask";
 import { Password } from "primereact/password";
-import { Divider } from 'primereact/divider';
+import { Divider } from "primereact/divider";
 import { RadioButton } from "primereact/radiobutton";
 
-class CadastroUsuario extends React.Component {
-  state = {
-    nome: "",
-    login: "",
-    email: "",
-    senha: "",
-    confirmarSenha: "",
-    dataNasc: null,
-    sexo: "",
-    cpf: 0,
-  };
+export default function CadastroUsuario(props) {
+  const [usuarioCadastro, setUsuarioCadastro] = useState(
+    props.usuario
+  );
 
-  onTrigger = () => {
-    this.props.parentCallback(this.state);
+  function onTriggerUsuarioCadastro() {
+    props.parentCallback(usuarioCadastro);
   }
 
-  render() {
-    const header = <h6>Insire sua senha</h6>;
-    const footer = (
-        <React.Fragment>
-            <Divider />
-            <p className="mt-2">Sugestões</p>
-            <ul className="pl-2 ml-2 mt-0" style={{lineHeight: '1.5'}}>
-                <li>Pelo menos uma letra minúscula</li>
-                <li>Pelo menos uma letra maiúscula</li>
-                <li>Pelo menos um número</li>
-                <li>No mínimo 8 caracteres</li>
-            </ul>
-        </React.Fragment>
-    );
+  const header = <h6>Insire sua senha</h6>;
+  const footer = (
+    <React.Fragment>
+      <Divider />
+      <p className="mt-2">Sugestões</p>
+      <ul className="pl-2 ml-2 mt-0" style={{ lineHeight: "1.5" }}>
+        <li>Pelo menos uma letra minúscula</li>
+        <li>Pelo menos uma letra maiúscula</li>
+        <li>Pelo menos um número</li>
+        <li>No mínimo 8 caracteres</li>
+      </ul>
+    </React.Fragment>
+  );
 
-    return (
-      <>
-        <div className="col-12 bg-blue d-flex flex-wrap align-items-center justify-content-center rounded" style={{height:'40rem'}}>
-          <h1 className="col-12 text-center text-white py-1">Cadastro - Usuário</h1>
-          <div className="col-md-12 col-lg-6 px-4">
-            <div className="col-12 text-white py-4">
-              <p>Nome</p>
-              <InputText
-                value={this.state.nome}
-                onChange={(e) => {this.setState({ nome: e.target.value }); this.onTrigger()}}
-                className="col-12 border border-0 rounded-pill"
-                type="text"
-                placeholder="digite seu nome..."
-              />
-            </div>
-            <div className="col-12 text-white py-4">
-              <p>Login</p>
-              <InputText
-                value={this.state.login}
-                onChange={(e) => {this.setState({ login: e.target.value }); this.onTrigger()}}
-                className="col-12 border border-0 rounded-pill"
-                type="text"
-                placeholder="digite seu usuario..."
-              />
-            </div>
-            <div className="col-12 text-white py-4">
-              <p>Email</p>
-              <InputText
-                value={this.state.email}
-                onChange={(e) => {this.setState({ email: e.target.value }); this.onTrigger()}}
-                className="col-12 border border-0 rounded-pill"
-                type="text"
-                placeholder="digite seu email..."
-              />
-            </div>
-            <div className="col-12 text-white py-4">
-              <p>Senha</p>
-              <Password
-                className="col-12 border border-0 rounded-pill"
-                value={this.state.senha}
-                onChange={(e) => {this.setState({ senha: e.target.value }); this.onTrigger()}}
-                feedback={true}
-                header={header} 
-                footer={footer}
-                weakLabel="Fraca"
-                mediumLabel="Media"
-                strongLabel="Forte"
-                toggleMask
-              />
-            </div>
+  return (
+    <>
+      <div
+        className="col-12 bg-blue d-flex flex-wrap align-items-center justify-content-center rounded"
+        style={{ height: "40rem" }}
+      >
+        <h1 className="col-12 text-center text-white py-1">
+          Cadastro - Usuário
+        </h1>
+        <div className="col-md-12 col-lg-6 px-4">
+          <div className="col-12 text-white py-4">
+            <p>Nome</p>
+            <InputText
+              value={usuarioCadastro.nome}
+              onChange={(e) => {
+                setUsuarioCadastro({ nome: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+              className="col-12 border border-0 rounded-pill"
+              type="text"
+              placeholder="digite seu nome..."
+            />
           </div>
-          <div className="col-md-12 col-lg-6 px-4">
-            <div className="col-12 text-white py-4">
-              <p>Data Nascimento</p>
-              <Calendar
-                dateFormat="dd/mm/yy"
-                className="col-12 border border-0 rounded-pill py-0"
-                value={this.state.dataNasc}
-                onChange={(e) => {this.setState({ dataNasc: e.target.value }); this.onTrigger()}}
-              ></Calendar>
-            </div>
-            <div className="row text-white py-4">
-              <p className="col-12">Sexo</p>
-              <div className="col-12 d-flex flex-wrap py-2">
-                <div className="col-4 field-radiobutton py-1">
-                  <RadioButton
-                    inputId="sexo"
-                    name="sexo"
-                    value="F"
-                    onChange={(e) => {this.setState({ sexo: e.target.value }); this.onTrigger()}}
-                    checked={this.state.sexo === "F"}
-                  />
-                  <label htmlFor="sexo" className="px-2">
-                    Feminino
-                  </label>
-                </div>
-                <div className="col-4 field-radiobutton py-1">
-                  <RadioButton
-                    inputId="sexo"
-                    name="sexo"
-                    value="M"
-                    onChange={(e) => {this.setState({ sexo: e.target.value }); this.onTrigger()}}
-                    checked={this.state.sexo === "M"}
-                  />
-                  <label htmlFor="sexo" className="px-3">
-                    Masculino
-                  </label>
-                </div>
-              </div>
-            </div>
-            <div className="col-12 text-white py-4">
-              <p>CPF</p>
-              <InputMask
-                className="col-12 border border-0 rounded-pill"
-                mask="999.999.999-99"
-                value={this.state.cpf}
-                onChange={(e) => {this.setState({ cpf: e.target.value }); this.onTrigger()}}
-              ></InputMask>
-            </div>
-            <div className="col-12 text-white py-4">
-              <p>Confirmar Senha</p>
-              <Password
-                className="col-12 border border-0 rounded-pill"
-                value={this.state.confirmarSenha}
-                onChange={(e) => {this.setState({ confirmarSenha: e.target.value }); this.onTrigger()}}
-                feedback={false}
-                toggleMask
-              />
-            </div>
+          <div className="col-12 text-white py-4">
+            <p>Login</p>
+            <InputText
+              value={usuarioCadastro.login}
+              onChange={(e) => {
+                setUsuarioCadastro({ login: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+              className="col-12 border border-0 rounded-pill"
+              type="text"
+              placeholder="digite seu usuario..."
+            />
+          </div>
+          <div className="col-12 text-white py-4">
+            <p>Email</p>
+            <InputText
+              value={usuarioCadastro.email}
+              onChange={(e) => {
+                setUsuarioCadastro({ email: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+              className="col-12 border border-0 rounded-pill"
+              type="text"
+              placeholder="digite seu email..."
+            />
+          </div>
+          <div className="col-12 text-white py-4">
+            <p>Senha</p>
+            <Password
+              className="col-12 border border-0 rounded-pill"
+              value={usuarioCadastro.senha}
+              onChange={(e) => {
+                setUsuarioCadastro({ senha: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+              feedback={true}
+              header={header}
+              footer={footer}
+              weakLabel="Fraca"
+              mediumLabel="Media"
+              strongLabel="Forte"
+              toggleMask
+            />
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="col-md-12 col-lg-6 px-4">
+          <div className="col-12 text-white py-4">
+            <p>Data Nascimento</p>
+            <Calendar
+              dateFormat="dd/mm/yy"
+              className="col-12 border border-0 rounded-pill py-0"
+              value={usuarioCadastro.dataNascimento}
+              onChange={(e) => {
+                setUsuarioCadastro({ dataNascimento: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+            ></Calendar>
+          </div>
+          <div className="row text-white py-4">
+            <p className="col-12">Sexo</p>
+            <div className="col-12 d-flex flex-wrap py-2">
+              <div className="col-4 field-radiobutton py-1">
+                <RadioButton
+                  inputId="sexo"
+                  name="sexo"
+                  value="FEMININO"
+                  onChange={(e) => {
+                    setUsuarioCadastro({ sexo: e.target.value });
+                    onTriggerUsuarioCadastro();
+                  }}
+                  checked={usuarioCadastro.sexo === "FEMININO"}
+                />
+                <label htmlFor="sexo" className="px-2">
+                  Feminino
+                </label>
+              </div>
+              <div className="col-4 field-radiobutton py-1">
+                <RadioButton
+                  inputId="sexo"
+                  name="sexo"
+                  value="MASCULINO"
+                  onChange={(e) => {
+                    setUsuarioCadastro({ sexo: e.target.value });
+                    onTriggerUsuarioCadastro();
+                  }}
+                  checked={usuarioCadastro.sexo === "MASCULINO"}
+                />
+                <label htmlFor="sexo" className="px-3">
+                  Masculino
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 text-white py-4">
+            <p>CPF</p>
+            <InputMask
+              className="col-12 border border-0 rounded-pill"
+              mask="999.999.999-99"
+              value={usuarioCadastro.cpf}
+              onChange={(e) => {
+                setUsuarioCadastro({ cpf: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+            ></InputMask>
+          </div>
+          <div className="col-12 text-white py-4">
+            <p>Confirmar Senha</p>
+            <Password
+              className="col-12 border border-0 rounded-pill"
+              value={usuarioCadastro.confirmarSenha}
+              onChange={(e) => {
+                setUsuarioCadastro({ confirmarSenha: e.target.value });
+                onTriggerUsuarioCadastro();
+              }}
+              feedback={false}
+              toggleMask
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default CadastroUsuario;
