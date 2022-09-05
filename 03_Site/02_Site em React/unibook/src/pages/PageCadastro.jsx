c1cf9ac7d86c1674265c6acc5036c8cf49a39d30
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-05 00:00:55 -0300

    FEAT(PageCadastro): Tornando cadastro funcional

--- 03_Site/02_Site em React/unibook/src/pages/PageCadastro.jsx
+++ 03_Site/02_Site em React/unibook/src/pages/PageCadastro.jsx
@@ -7,37 +7,47 @@ import { Steps } from "primereact/steps";
 import CadastroUsuario from "../components/CadastroUsuario";
 import CadastroEndereco from "../components/CadastroEndereco";
 import CadastroImagem from "../components/CadastroImagem";
-
 import Menu from "../components/Menu";
+
+import iconAddImage from "../templates/images/icon-add-image.png";
+import { mensagemErro, mensagemSucesso } from "../components/Toastr";
+
+import UsuarioService from "../app/service/usuarioService";
 
 import "../templates/styles/styles-cadastro.css";
 
 class PageCadastro extends React.Component {
-  state = {
-    tabcard: 0,
-    buttonLeft: true,
-    buttonRight: false,
-    usuario: {
-      nome: "",
-      dataNascimento: new Date(),
-      sexo: "",
-      cpf: "",
-      email: "",
-      senha: "",
-      confirmarSenha: "",
-      tipoUsuario: "COMPRADOR",
-      cep: "",
-      numeroResidencia: ["",""],
-      login: "",
-      foto: [1],
-    },
-    enderecoPesquisa: {
-      cidade: "",
-      logradouro: "",
-      estado: "",
-    },
-  };
+  constructor(props) {
+    super(props);
 
+    this.service = new UsuarioService();
+
+    this.state = {
+      tabcard: 0,
+      buttonLeft: true,
+      buttonRight: false,
+      usuario: {
+        nome: "",
+        dataNascimento: new Date(),
+        sexo: "FEMININO",
+        cpf: "",
+        email: "",
+        senha: "",
+        confirmarSenha: "",
+        cep: "",
+        numeroResidencia: "",
+        complemento: "",
+        login: "",
+        foto: iconAddImage,
+      },
+      enderecoPesquisa: {
+        cidade: "",
+        logradouro: "",
+        estado: "",
+      },
+    };
+  }
+  
   disabled(index) {
     if (index === 0) {
       this.setState({ buttonLeft: true });
@@ -56,12 +66,45 @@ class PageCadastro extends React.Component {
   };
 
   handleCallbackCEP = (childData) => {
-    console.log(childData)
-    this.setState({ enderecoPesquisa: { ...this.state.enderecoPesquisa, ...childData } });
+    this.setState({
+      enderecoPesquisa: { ...this.state.enderecoPesquisa, ...childData },
+    });
+  };
+
+  handleCallbackFoto = (childData) => {
+    this.setState({
+      usuario : {...this.state.usuario, ...{ foto : childData}}
+    })
   }
+  
+  cadastrarUsuario = () => {
+    var usuarioCadastro = {
+      "nome": this.state.usuario.nome,
+      "dataNascimento": Date.parse(this.state.usuario.dataNascimento),
+      "sexo": this.state.usuario.sexo,
+      "cpf": this.state.usuario.cpf,
+      "email": this.state.usuario.email,
+      "senha": this.state.usuario.senha,
+      "tipoUsuario": "VENDEDOR",
+      "cep": this.state.usuario.cep.replace(/-/,""),
+      "numeroResidencia": this.state.usuario.numeroResidencia + " " +  this.state.usuario.complemento ,
+      "login": this.state.usuario.login
+    }
 
+    this.service.salvarUsuario(usuarioCadastro).then((response) => {
+      mensagemSucesso("USUÁRIO CADASTRADO")
+      console.log(response)
+      setInterval(1000000)
+      window.location.href = "/login"
+    })
+    .catch((erro) => {
+      mensagemErro(erro.response.data.message);
+      return;
+    });
+  };
+  
   items = [{ label: "Usuario" }, { label: "Endereço" }, { label: "Imagem" }];
-
+  
   render() {
     const clickLeft = () => {
       if (this.state.tabcard === 0) {
@@ -131,7 +174,11 @@ class PageCadastro extends React.Component {
                       />
                     </TabPanel>
                     <TabPanel header="Imagem">
-                      <CadastroImagem className="col-12" />
+                      <CadastroImagem
+                        className="col-12"
+                        parentCallbackFoto={this.handleCallbackFoto}
+                        foto={this.state.usuario.foto}
+                      />
                     </TabPanel>
                   </TabView>
                   <div className="col-12 d-flex justify-content-end pe-4 my-2">
@@ -139,6 +186,9 @@ class PageCadastro extends React.Component {
                       className="bg-orange rounded-pill border border-dark border-2 fw-bold px-4 pb-2 text-dark hover-yellow"
                       label="Finalizar Cadastro"
                       hidden={!this.state.buttonRight}
+                      onClick={() => {
+                        this.cadastrarUsuario();
+                      }}
                     />
                   </div>
                 </div>
