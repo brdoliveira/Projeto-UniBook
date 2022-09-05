9a6991c6bbffdf31dfaa5128bde3c84c0bb32316
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-04 23:52:23 -0300

    FEAT(app): Habilitando permissão de CORS

--- 03_Site/02_Site em React/unibook/app.js
+++ 03_Site/02_Site em React/unibook/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "build")));
 
