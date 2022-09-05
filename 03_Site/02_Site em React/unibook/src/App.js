5016d66994b05b8fa1eaf17abedef6d26a906e24
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-04 23:53:41 -0300

    FEAT(App): importando estilo do toastrr

--- 03_Site/02_Site em React/unibook/src/App.js
+++ 03_Site/02_Site em React/unibook/src/App.js
@@ -6,6 +6,8 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
 
+import 'toastr/build/toastr.css' // toastr css
+
 import Rotas from "./routes";
 
 function App() {
