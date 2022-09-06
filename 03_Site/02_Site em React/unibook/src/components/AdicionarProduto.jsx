7ad4f0f1043a99ff582435300364264827a5d3e5
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-05 23:52:52 -0300

    FEAT(AdicionanrProduto): Modificando estrutura do Adicionar produto

--- 03_Site/02_Site em React/unibook/src/components/AdicionarProduto.jsx
+++ 03_Site/02_Site em React/unibook/src/components/AdicionarProduto.jsx
@@ -1,184 +1,170 @@
-import React from "react";
-import iconAddImage from "../templates/images/icon-add-image.png";
+import React, { useState } from "react";
 
 // import { ScrollPanel } from "primereact/scrollpanel";
 // import { Chips } from "primereact/chips";
+
+import iconAddImage from "../templates/images/icon-add-image.png";
+import { mensagemErro, mensagemSucesso } from "./Toastr";
+import BookService from "../app/service/bookService";
 import { FileUpload } from "primereact/fileupload";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 
-import BookService from "../app/service/bookService";
-import { mensagemErro , mensagemSucesso } from "./Toastr";
-
 import "../templates/styles/styles-cadastro.css";
 
-class AdicionarProduto extends React.Component {
-  constructor(){
-    super()
-    this.service = new BookService();
-  }
+export default function AdicionarProduto(props) {
+  const [livroInformacoes,setLivroInformacoes] = useState(props.livro)
 
-  state = {
-    nome: "",
-    preco: 0,
-    descricao: "",
-    dataLanc: "",
-    editora: "",
-    isbn: "",
-    idioma: "",
-    quantidade: "",
-    estado : "",
-    imgPath: iconAddImage,
-    etiquetas: [],
+  const consultarLivro = async (isbn) => {
+    const service = new BookService();
+    let dadosLivros = await service.consultarLivro(
+      isbn === "" ? "0" : isbn
+    );
+
+    if (dadosLivros.sucesso) {
+      mensagemSucesso("ISBN do Livro encontrado com sucesso");
+      console.log(dadosLivros.dados);
+    } else {
+      mensagemErro("ISBN do Livro não encontrado");
+    }
   };
 
-render() {
-    const consultarLivro = async (isbn) => {
-      console.log(isbn)
-      let dadosLivros = await this.service.consultarLivro(isbn === "" ? "0" : isbn)
-      
-      if(dadosLivros.sucesso){
-        mensagemSucesso("ISBN do Livro encontrado com sucesso")
-        console.log(dadosLivros.dados)
-      }else{
-        mensagemErro("ISBN do Livro não encontrado")
-      }
-    };
+  const customBase64Uploader = async (event) => {
+    if (livroInformacoes.imgPath === iconAddImage) {
+      var base64data;
+      const file = event.files[0];
+      const reader = new FileReader();
+      let blob = await fetch(file.objectURL).then((r) => r.blob());
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => {
+        base64data = reader.result;
+        setLivroInformacoes({ imgPath: base64data });
+      };
+      return;
+    }
+  };
 
-    const customBase64Uploader = async (event) => {
-      if (this.state.imgPath === iconAddImage) {
-        var base64data;
-        const file = event.files[0];
-        const reader = new FileReader();
-        let blob = await fetch(file.objectURL).then((r) => r.blob());
-        reader.readAsDataURL(blob);
-        reader.onloadend = () => {
-          base64data = reader.result;
-          this.setState({ imgPath: base64data });
-        };
-        return;
-      }
-    };
-
-    return (
-      <>
-        <div
-          className="col-12 bg-blue d-flex flex-wrap align-items-center justify-content-center rounded"
-          style={{ height: "50vh" }}
-        >
-          <h1 className="col-12 text-center text-white py-1">
-            Cadastro - Produto
-          </h1>
-          <div className="col-12 col-md-4 px-4  d-flex flex-wrap align-items-center justify-content-center">
-            <div
-              className="col-12 bg-white d-flex flex-wrap align-items-center justify-content-center"
-              style={{ height: "35vh", width: "60%" }}
-            >
-              <img
-                src={this.state.imgPath}
-                alt=""
-                id="imgPhoto"
-                style={{ height: "29vh", width: "90%" }}
-              />
-            </div>
-            <div className="col-12 d-flex justify-content-center py-1">
-              <FileUpload
-                mode="basic"
-                name="demo[]"
-                accept="image/*"
-                customUpload
-                uploadHandler={customBase64Uploader}
-                auto
-              />
-            </div>
+  return (
+    <>
+      <div
+        className="col-12 bg-blue d-flex flex-wrap align-items-center justify-content-center rounded"
+        style={{ height: "50vh" }}
+      >
+        <h1 className="col-12 text-center text-white py-1">
+          Cadastro - Produto
+        </h1>
+        <div className="col-12 col-md-4 px-4  d-flex flex-wrap align-items-center justify-content-center">
+          <div
+            className="col-12 bg-white d-flex flex-wrap align-items-center justify-content-center"
+            style={{ height: "35vh", width: "60%" }}
+          >
+            <img
+              src={livroInformacoes.imgPath}
+              alt=""
+              id="imgPhoto"
+              style={{ height: "29vh", width: "90%" }}
+            />
           </div>
-          <div className="col-12 col-md-8 px-4" hidden={this.props.page}>
-            <div className="col-12 text-white py-4 d-flex flex-wrap px-2">
-              <div className="col-6 text-white py-4 pe-1">
-                <p>ISBN</p>
-                <div className="p-inputgroup">
-                  <InputText
-                    value={this.state.isbn}
-                    onChange={(e) => this.setState({ isbn: e.target.value })}
-                    className="col-12 border border-0 rounded-pill rounded-end"
-                    type="text"
-                    placeholder="ISBN do livro..."
-                  />
-                  <Button
-                    icon="pi pi-search"
-                    className="rounded-start rounded-pill"
-                    onClick={ () => {consultarLivro(this.state.isbn)}}
-                  />
-                </div>
-              </div>
-              <div className="col-6 text-white py-4 ps-1">
-                <p>Nome do Livro</p>
+          <div className="col-12 d-flex justify-content-center py-1">
+            <FileUpload
+              mode="basic"
+              name="demo[]"
+              accept="image/*"
+              customUpload
+              uploadHandler={customBase64Uploader}
+              auto
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-8 px-4" hidden={this.props.page}>
+          <div className="col-12 text-white py-4 d-flex flex-wrap px-2">
+            <div className="col-6 text-white py-4 pe-1">
+              <p>ISBN</p>
+              <div className="p-inputgroup">
                 <InputText
-                  value={this.state.nome}
-                  onChange={(e) => this.setState({ nome: e.target.value })}
-                  className="col-12 border border-0 rounded-pill"
+                  value={livroInformacoes.isbn}
+                  onChange={(e) => this.setState({ isbn: e.target.value })}
+                  className="col-12 border border-0 rounded-pill rounded-end"
                   type="text"
-                  placeholder="Nome do livro..."
+                  placeholder="ISBN do livro..."
+                />
+                <Button
+                  icon="pi pi-search"
+                  className="rounded-start rounded-pill"
+                  onClick={() => {
+                    consultarLivro(livroInformacoes.isbn);
+                  }}
                 />
               </div>
             </div>
-            <div className="col-12 text-white py-4 px-2">
-              <p>Descrição do Livro</p>
+            <div className="col-6 text-white py-4 ps-1">
+              <p>Nome do Livro</p>
               <InputText
-                value={this.state.descricao}
-                onChange={(e) => this.setState({ descricao: e.target.value })}
+                value={livroInformacoes.nome}
+                onChange={(e) => setLivroInformacoes({ nome: e.target.value })}
                 className="col-12 border border-0 rounded-pill"
                 type="text"
-                placeholder="Descrição do livro..."
+                placeholder="Nome do livro..."
               />
             </div>
           </div>
-          <div className="col-12 col-md-8 px-4" hidden={!this.props.page}>
-            <div className="col-12 text-white pt-4 d-flex">
-              <div className="col-6 text-white py-4 pe-2">
-                <p>Ano de Lancamento</p>
-                <InputText
-                  value={this.state.data}
-                  onChange={(e) => this.setState({ data: e.target.value })}
-                  className="col-12 border border-0 rounded-pill"
-                  type="text"
-                  placeholder="Data de lancamento..."
-                />
-              </div>
-              <div className="col-6 text-white py-4 ps-2">
-                <p>Editora ou Autor</p>
-                <InputText
-                  value={this.state.autor}
-                  onChange={(e) => this.setState({ autor: e.target.value })}
-                  className="col-12 border border-0 rounded-pill"
-                  type="text"
-                  placeholder="Editora do livro..."
-                />
-              </div>
+          <div className="col-12 text-white py-4 px-2">
+            <p>Descrição do Livro</p>
+            <InputText
+              value={livroInformacoes.descricao}
+              onChange={(e) => setLivroInformacoes({ descricao: e.target.value })}
+              className="col-12 border border-0 rounded-pill"
+              type="text"
+              placeholder="Descrição do livro..."
+            />
+          </div>
+        </div>
+        <div className="col-12 col-md-8 px-4" hidden={!this.props.page}>
+          <div className="col-12 text-white pt-4 d-flex">
+            <div className="col-6 text-white py-4 pe-2">
+              <p>Ano de Lancamento</p>
+              <InputText
+                value={livroInformacoes.data}
+                onChange={(e) => setLivroInformacoes({ data: e.target.value })}
+                className="col-12 border border-0 rounded-pill"
+                type="text"
+                placeholder="Data de lancamento..."
+              />
             </div>
-            <div className="col-12 text-white pt-4 d-flex">
-              <div className="col-6 text-white py-4 pe-2">
-                <p>Estado de Uso</p>
-                <InputText
-                  value={this.state.data}
-                  onChange={(e) => this.setState({ data: e.target.value })}
-                  className="col-12 border border-0 rounded-pill"
-                  type="text"
-                  placeholder="Data de lancamento..."
-                />
-              </div>
-              <div className="col-6 text-white py-4 ps-2">
-                <p>Quantidade</p>
-                <InputText
-                  value={this.state.quantidade}
-                  onChange={(e) => this.setState({ quantidade: e.target.value })}
-                  className="col-12 border border-0 rounded-pill"
-                  type="text"
-                  placeholder="Quantidade..."
-                />
-              </div>
+            <div className="col-6 text-white py-4 ps-2">
+              <p>Editora ou Autor</p>
+              <InputText
+                value={livroInformacoes.autor}
+                onChange={(e) => setLivroInformacoes({ autor: e.target.value })}
+                className="col-12 border border-0 rounded-pill"
+                type="text"
+                placeholder="Editora do livro..."
+              />
             </div>
-            {/* <div className="col-12 text-white pt-4">
+          </div>
+          <div className="col-12 text-white pt-4 d-flex">
+            <div className="col-6 text-white py-4 pe-2">
+              <p>Estado de Uso</p>
+              <InputText
+                value={livroInformacoes.estado}
+                onChange={(e) => setLivroInformacoes({ estado: e.target.value })}
+                className="col-12 border border-0 rounded-pill"
+                type="text"
+                placeholder="Data de lancamento..."
+              />
+            </div>
+            <div className="col-6 text-white py-4 ps-2">
+              <p>Quantidade</p>
+              <InputText
+                value={livroInformacoes.quantidade}
+                onChange={(e) => setLivroInformacoes({ quantidade: e.target.value })}
+                className="col-12 border border-0 rounded-pill"
+                type="text"
+                placeholder="Quantidade..."
+              />
+            </div>
+          </div>
+          {/* <div className="col-12 text-white pt-4">
               <p>Etiquetas</p>
               <ScrollPanel
                 className="col-12 scroll rounded"
@@ -194,11 +180,8 @@ render() {
                 />
               </ScrollPanel>
             </div> */}
-          </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
-export default AdicionarProduto;
