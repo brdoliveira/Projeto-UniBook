66f9badcdc6e3205988e74068d1e773e842cc008
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-07 23:51:42 -0300

    FEAT(apiservice): Criando 2 baseUrl

--- 03_Site/02_Site em React/unibook/src/app/apiservice.js
+++ 03_Site/02_Site em React/unibook/src/app/apiservice.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: "http://44.199.166.133:8004/"
+    baseURL: "http://localhost:8080/"
+    // baseURL: "http://44.199.166.133:8004"
 })
 
 
