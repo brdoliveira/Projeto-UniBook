a18256ac87ec1cbe96a0cc45de5a8aa5f62fddfe
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-18 23:42:10 -0300

    FEAT(Administrador): Criando estrutura Inicial do componente Administrador

---
+++ 04_Site/02_Site em React/unibook/src/components/Administrador.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Admnistrador extends React.Component{
+    super(){}
+
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
+
+export default Admnistrador;
