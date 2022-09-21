e04e0fce2ed79ddc2c1c85ac596eec24b9a2eb29
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-20 23:26:38 -0300

    FEAT(AdministradorAnalytics): Criando estrutura inicial do componentes de administracao clientes

---
+++ 04_Site/02_Site em React/unibook/src/components/AdministradorCliente.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class AdministradorCliente extends React.Component {
+  super() {}
+
+  render() {
+    return (
+      <>
+        <div className="col-12 text-center text-dark pt-5">
+          <h1>Administração dos Clientes</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default AdministradorCliente;
