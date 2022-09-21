62aa4dcbe2644314f501be7da0c20ff1f84c86fa
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-20 23:25:45 -0300

    FEAT(AdministradorAnalytics): Criando estrutura inicial da tela de analytics

---
+++ 04_Site/02_Site em React/unibook/src/components/AdministradorAnalytics.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class AdministradorAnalytics extends React.Component {
+  super() {}
+
+  render() {
+    return (
+      <>
+        <div className="col-12 text-center text-dark pt-5">
+          <h1>Analitycs Administrações</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default AdministradorAnalytics;
