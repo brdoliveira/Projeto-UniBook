75bc22c66768086b18714ba04e79c67dfa89fedc
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-21 23:52:10 -0300

    FEAT(RemoverEditarUsuarioItem): Criando item do remover editar usuario

---
+++ 04_Site/02_Site em React/unibook/src/components/RemoverEditarUsuarioItem.jsx
@@ -0,0 +1,13 @@
+export default function RemoverEditarUsuarioItem() {
+  return (
+    <>
+      <div className="col-12 rounded bg-white my-2 d-flex flex-wrap p-2" style={{ height: "5rem" }}>
+        <div className="col-8">
+            jjj
+        </div>
+        <div className="col-2">dddddddd</div>
+        <div className="col-2">ddd</div>
+      </div>
+    </>
+  );
+}
