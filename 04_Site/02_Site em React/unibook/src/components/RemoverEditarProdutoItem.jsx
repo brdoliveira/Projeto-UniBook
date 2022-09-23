a91f7498ea5694df67a46dea34150e3bdf61aed4
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-23 20:40:56 -0300

    FEAT(RemoverEditarProdutoItem): Criando item do componente de editar e remover produto

---
+++ 04_Site/02_Site em React/unibook/src/components/RemoverEditarProdutoItem.jsx
@@ -0,0 +1,42 @@
+import React , { useState } from "react";
+
+import { Button } from "primereact/button";
+
+import { ConfirmDialog , confirmDialog } from 'primereact/confirmdialog';
+
+export default function RemoverEditarProdutoItem(props) {
+  const [produto] = useState(props.produto)
+
+  const confirm = () => {
+    confirmDialog({
+        message: `Tem certeza que deseja deletar o produto ${produto.nome}?`,
+        header: 'Confirmação',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () => console.log("Produto deletado !"),
+        reject: () => console.log("Produto não deletado !")
+    });
+  }
+ 
+  return (
+    <>
+      <div className="col-12 rounded bg-white my-2 d-flex flex-wrap p-2" style={{ height: "5rem" }}>
+        <div className="col-5">
+            <span className="fw-bold">Nome: </span>
+        </div>
+        <div className="col-3">
+          <span className="fw-bold">Editora: </span>
+        </div>
+        <div className="col-2">
+            <span className="fw-bold">Ano: </span>
+        </div>
+        <div className="col-2 d-flex align-items-center justify-content-end">
+          <Button icon="pi pi-pencil" className="rounded-pill p-button-secondary"/>
+          <div className="ps-2">
+          <Button icon="pi pi-trash" className="rounded-pill p-button-danger" onClick={confirm}/>
+          </div>
+        </div>
+      </div>
+      <ConfirmDialog />
+    </>
+  );
+}
