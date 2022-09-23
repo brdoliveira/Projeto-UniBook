0020219b374fe71231c21ea4f19d72d7c3f17016
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-23 20:36:54 -0300

    FEAT(AdministradorCliente): Adicionando novos componentes no componente de administrador

--- 04_Site/02_Site em React/unibook/src/components/AdministradorCliente.jsx
+++ 04_Site/02_Site em React/unibook/src/components/AdministradorCliente.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import RemoverEditarUsuario from "./RemoverEditarUsuario";
+import RemoverEditarProduto from "./RemoverEditarProduto";
+import AdicionarUsuario from "./AdicionarUsuario";
 
 class AdministradorCliente extends React.Component {
   super() {}
@@ -11,8 +13,14 @@ class AdministradorCliente extends React.Component {
         <div className="col-12 text-center text-dark pt-5">
           <h1>Administração dos Clientes</h1>
         </div>
-        <div className="col-12">
+        <div className="col-12 py-4">
           <RemoverEditarUsuario />
+        </div>
+        <div className="col-12 py-4">
+          <RemoverEditarProduto/>
+        </div>
+        <div className="col-12 py-4">
+          <AdicionarUsuario/>
         </div>
       </>
     );
