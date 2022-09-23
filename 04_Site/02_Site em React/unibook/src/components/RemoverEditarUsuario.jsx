1e2bb132449e7d4c66088629083d63d91cfdd3f4
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-23 20:39:16 -0300

    FEAT(RemoverEditarUsuario): Criando componente de editar e remover usuario

--- 04_Site/02_Site em React/unibook/src/components/RemoverEditarUsuario.jsx
+++ 04_Site/02_Site em React/unibook/src/components/RemoverEditarUsuario.jsx
@@ -6,7 +6,7 @@ export default function RemoverEditarUsuario(){
         <>
         <div className="col-12 rounded bg-blue d-flex justify-content-center align-items-center flex-wrap" style={{ height: "30rem" }}>
             <div className="col-11 text-white pt-4">
-                <h2>Remover Usuario ou Cliente</h2>
+                <h2>Remover Usuario</h2>
             </div>
             <ScrollPanel className="col-11 bg-bluelight pb-4 rounded py-4 ps-3" style={{ height: "20rem" }}>
                 <RemoverEditarUsuarioItem/>
