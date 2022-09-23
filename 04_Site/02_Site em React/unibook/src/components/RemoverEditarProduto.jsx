b5eaf4fbb117d7bf847e2ea1cd961df04ed80620
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-23 20:38:34 -0300

    FEAT(RemoverEditarProdutoItem): Criando item do componente de editar e remover produto

---
+++ 04_Site/02_Site em React/unibook/src/components/RemoverEditarProduto.jsx
@@ -0,0 +1,21 @@
+import { ScrollPanel } from 'primereact/scrollpanel';
+
+import RemoverEditarProdutoItem from "./RemoverEditarProdutoItem";
+
+export default function RemoverEditarProduto(){
+    return(
+        <>
+        <div className="col-12 rounded bg-blue d-flex justify-content-center align-items-center flex-wrap" style={{ height: "30rem" }}>
+            <div className="col-11 text-white pt-4">
+                <h2>Remover Produto</h2>
+            </div>
+            <ScrollPanel className="col-11 bg-bluelight pb-4 rounded py-4 ps-3" style={{ height: "20rem" }}>
+                <RemoverEditarProdutoItem/>
+                <RemoverEditarProdutoItem/>
+                <RemoverEditarProdutoItem/>
+                <RemoverEditarProdutoItem/>
+            </ScrollPanel>
+        </div>
+        </>
+    )
+}
