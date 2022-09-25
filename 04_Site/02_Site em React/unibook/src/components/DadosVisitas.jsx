09b222f196cb8a368c8c9b6cf2e71527f0a85154
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-09-25 19:12:50 -0300

    FEAT(DadosVisitas): Componente com os dados de visitas no sites

---
+++ 04_Site/02_Site em React/unibook/src/components/DadosVisitas.jsx
@@ -0,0 +1,71 @@
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
+
+export default function DadosVisitas() {
+  const [selectedPeriod, setSelectedPeriod] = useState(null);
+
+  const period = [
+    { name: "4 Junho - 31 Julho", code: "NY" },
+    { name: "4 Junho - 31 Julho", code: "RM" },
+    { name: "4 Junho - 31 Julho", code: "LDN" },
+    { name: "4 Junho - 31 Julho", code: "IST" },
+    { name: "4 Junho - 31 Julho", code: "PRS" },
+  ];
+
+  const onPeriodChange = (e) => {
+    setSelectedPeriod(e.value);
+  };
+
+  return (
+    <>
+      <div className="col-12">
+        <div className="col-12 d-flex align-items-center justify-content-end px-5">
+          <div className="col-2">
+            <Dropdown
+              value={selectedPeriod}
+              options={period}
+              onChange={onPeriodChange}
+              optionLabel="name"
+              placeholder="4 Junho - 31 Julho"
+              className="w-100"
+            />
+          </div>
+        </div>
+        <div className="col-12 d-flex flex-wrap py-3">
+          <div className="col-3 px-5">
+            <div className="col-12 text-center text-dark fw-bold">
+              <h4>Visitas Mês</h4>
+            </div>
+            <div className="col-12 text-center p-2 bg-blue rounded text-white display-6">
+              <span className="fw-bold">140</span>
+            </div>
+          </div>
+          <div className="col-3 px-5">
+            <div className="col-12 text-center text-dark fw-bold">
+              <h4>Visitas Últimos 7 dias</h4>
+            </div>
+            <div className="col-12 text-center p-2 bg-blue rounded text-white display-6">
+              <span className="fw-bold">140</span>
+            </div>
+          </div>
+          <div className="col-3 px-5">
+            <div className="col-12 text-center text-dark fw-bold">
+              <h4>Cadastros Parciais</h4>
+            </div>
+            <div className="col-12 text-center p-2 bg-blue rounded text-white display-6">
+              <span className="fw-bold">140</span>
+            </div>
+          </div>
+          <div className="col-3 px-5">
+            <div className="col-12 text-center text-dark fw-bold">
+              <h4>Cadastros Finalizados</h4>
+            </div>
+            <div className="col-12 text-center p-2 bg-blue rounded text-white display-6">
+              <span className="fw-bold">140</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
