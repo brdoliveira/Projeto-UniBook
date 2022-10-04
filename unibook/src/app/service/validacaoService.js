4912f6b30bbcfbb5e1224838044d1a5d1ce035e0
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-03 23:50:28 -0300

    FEAT(validacaoService): Criando novas validações e melhorando codigo

--- unibook/src/app/service/validacaoService.js
+++ unibook/src/app/service/validacaoService.js
@@ -1,40 +1,69 @@
 
 class ValidacaoService{
 
-    validarCampo(nomeCampo,valor){
-        if(nomeCampo === null){ 
-            console.log("nome do campo é nulo")
-        }else if(valor === null){ 
-            console.log("valor é nulo")
-        }else{ 
-            console.log("campos validados")
-        }
+    validarCampo(valor){
+        return !valor ? false : true 
     }
 
     validarData(data){
-        dataHoje = Date()
-
-        if( data >= dataHoje){
-            console.log("data invalida!")
-        }else{
-            console.log("data validada!")
-        }
+        return data >= Date() ? true : false
     }
 
-    validaTamanhoCampo(nomeCampo,valor,tamanhoMin, tamanhoMax){
-        this.validarCampo(nomeCampo,valor)
-
-        if(valor.length >= tamanhoMin || valor.length <= tamanhoMax){
-            console.log("campo validado!")
-        }else{
-            console.log("campo invalido!")
-        }
+    validaTamanhoCampo(valor,tamanhoMin, tamanhoMax){
+        return (valor.length >= tamanhoMin || valor.length <= tamanhoMax) ? true : false
     }
 
-    validarValor(nomeCampo,valor,valorMin){
-        if(valor < valorMin){
-            console.log("valor invalido!")
-        }
+    validarValor(valor,valorMin){
+        return (valor > valorMin) ? true : false
+    }
+
+    validarCpf(cpf){
+        const blacklist = [
+            '00000000000',
+            '11111111111',
+            '22222222222',
+            '33333333333',
+            '44444444444',
+            '55555555555',
+            '66666666666',
+            '77777777777',
+            '88888888888',
+            '99999999999'
+          ]
+        
+          const cleaned = cpf.replace(/\./g, '').replace(/\-/g, '')
+        
+          if (blacklist.includes(cleaned)) {
+            return false
+          }
+        
+          let sum = 0
+          let check = 0
+        
+          // First digit
+          for (let i = 1; i <= 9; i++) {
+            sum += parseInt(cleaned.substring(i - 1, i)) * (11 - i)
+          }
+        
+          if (
+            ((check = (10 * sum) % 11),
+            (10 != check && 11 != check) || (check = 0),
+            check != parseInt(cleaned.substring(9, 10)))
+          ) {
+            return false
+          }
+        
+          // Second digit
+          sum = 0
+          for (let i = 1; i <= 10; i++) {
+            sum += parseInt(cleaned.substring(i - 1, i)) * (12 - i)
+          }
+        
+          return (
+            (check = (10 * sum) % 11),
+            (10 != check && 11 != check) || (check = 0),
+            check == parseInt(cleaned.substring(10, 11))
+          )
     }
 }
 
