ecd44319f642516322fa4d59ef4f2cfb6b19a46a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-04 21:24:13 -0300

    FEAT(produtoAnunciadoService):  Criando método para validar formulario

--- unibook/src/app/service/produtosAnunciadosService.js
+++ unibook/src/app/service/produtosAnunciadosService.js
@@ -1,46 +1,89 @@
 import ApiService from "../apiservice";
 
+import ValidacaoService from "./validacaoService";
+import ErroValidacao from "../exception/erroValidacao";
+
 class ProdutosAnunciadosService extends ApiService {
   constructor() {
     super("/produtos-anunciados");
+    this.validacao = new ValidacaoService();
   }
 
-  salvarProduto(produto){
-    return this.postObject("",produto);
+  salvarProduto(produto) {
+    return this.postObject("", produto);
   }
 
-  validarProduto(){
-    
+  validarProduto(produtoInserido) {
+    const erros = [];
+
+    if (this.validacao.validarCampo(produtoInserido.titulo)) {
+      erros.push("Titulo do livro inválido");
+    }
+
+    if (this.validacao.validarCampo(produtoInserido.autor)) {
+      erros.push("Nome do autor inválido");
+    }
+
+    if (
+      this.validacao.validarCampo(produtoInserido.valor) ||
+      this.validacao.validarValor(produtoInserido.valor)
+    ) {
+      erros.push("Valor do livro inválido");
+    }
+
+    if (this.validacao.validarCampo(produtoInserido.estadoUso)) {
+      erros.push("Estado de uso inválido");
+    }
+
+    if (
+      this.validacao.validarCampo(produtoInserido.anoPublicacao) ||
+      this.validacao.validarData(produtoInserido.anoPublicacao)
+    ) {
+      erros.push("Ano publicacao inválido");
+    }
+
+    if (this.validacao.validarCampo(produtoInserido.descricao)) {
+      erros.push("Descrição inválida");
+    }
+
+    if (this.validacao.validarCampo(produtoInserido.idioma)) {
+      erros.push("Idioma inválida");
+    }
+
+    if (erros && erros.length > 0) {
+      throw new ErroValidacao(erros);
+    }
   }
 
-  listarTodos(){
-    return this.get("/listar-todos")
+  listarTodos() {
+    return this.get("/listar-todos");
   }
 
-  listarTodosPilha(){
-    return this.get("/listar-todos-pilha")
+  listarTodosPilha() {
+    return this.get("/listar-todos-pilha");
   }
 
-  pesquisarProdutos(idProduto){
-    return this.get(`/listar-todos?idProduto=${idProduto}`)
+  pesquisarProdutos(idProduto) {
+    return this.get(`/listar-todos?idProduto=${idProduto}`);
   }
 
-  foto(idProdutoAnuncio){
-    return this.get(`/foto?idProdutoAnuncio=${idProdutoAnuncio}`)
+  foto(idProdutoAnuncio) {
+    return this.get(`/foto?idProdutoAnuncio=${idProdutoAnuncio}`);
   }
 
-  alterarFoto(idProdutoAnuncio,novaFoto){
-    return this.post(`/alterar-foto?idProdutoAnuncio=${idProdutoAnuncio}&novaFoto=${novaFoto}`)
+  alterarFoto(idProdutoAnuncio, novaFoto) {
+    return this.post(
+      `/alterar-foto?idProdutoAnuncio=${idProdutoAnuncio}&novaFoto=${novaFoto}`
+    );
   }
 
-  listarAnunciosPorVendedor(nomeUsuario){
-    return this.get(`/listar-anuncios-por-vendedor?nomeUsuario=${nomeUsuario}`)
+  listarAnunciosPorVendedor(nomeUsuario) {
+    return this.get(`/listar-anuncios-por-vendedor?nomeUsuario=${nomeUsuario}`);
   }
 
-  listarAnunciosPorTitulo(titulo){
-    return this.get(`/listar-anuncios-por-titulo?titulo=${titulo}`)
+  listarAnunciosPorTitulo(titulo) {
+    return this.get(`/listar-anuncios-por-titulo?titulo=${titulo}`);
   }
-
 }
 
 export default ProdutosAnunciadosService;
