61fabffe3a8255d5a000b1b5eb7c58b73ea23e8b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-06 18:04:45 -0300

    FEAT(PageFaleConosco): Alinhando tela de fale conosco

--- unibook/src/pages/PageFaleConosco.jsx
+++ unibook/src/pages/PageFaleConosco.jsx
@@ -4,9 +4,11 @@ import FaleConosco from "../components/FaleConosco";
 function PageFaleConosco() {
   return (
     <>
+      <div className="min-vh-100">
       <Menu />
-      <div className="col-12 d-flex justify-content-center align-items-center pt-5 mt-5">
+      <div className="col-12 d-flex justify-content-center align-items-center pt-5">
         <FaleConosco />
+      </div>
       </div>
     </>
   );
