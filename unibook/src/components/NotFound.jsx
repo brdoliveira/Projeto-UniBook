8375347015a5bb57f6998505040dc07b4e0efafa
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-06 18:07:28 -0300

    FEAT(NotFound): Alinhando tela de 404

--- unibook/src/components/NotFound.jsx
+++ unibook/src/components/NotFound.jsx
@@ -3,12 +3,9 @@ import "../templates/styles/styles-not-found.css";
 
 function NotFound() {
   return (
-    <div className="col-12 px-0 bg-white bg-height d-flex justify-content-center">
+    <div className="col-12 px-0 bg-white bg-height d-flex justify-content-center pt-5">
       <div className="bg-height d-flex align-items-center">
         <div className="d-flex flex-wrap justify-content-center align-items-center col-12 text-center text-white">
-          <div className="col-12">
-            <h1 className="text-dark">Pagina não Encontrada !</h1>
-          </div>
           <img className="image-not-found" src={iconNotFound} alt="Not Found" />
           <div className="col-12 d-flex align-items-center justify-content-center">
             <div className="col-12 col-md-6 col-lg-4">
