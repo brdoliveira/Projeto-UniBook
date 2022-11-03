d1d1ac4e1182a0468c40349b5de5e82f603b0fda
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-03 20:07:45 -0300

    FEAT(DadosVisitas): Tornando Metricas dinamicas

--- unibook/src/components/DadosVisitas.jsx
+++ unibook/src/components/DadosVisitas.jsx
@@ -1,8 +1,31 @@
 import { Dropdown } from "primereact/dropdown";
+import { useEffect } from "react";
 import { useState } from "react";
+import BiService from "../app/service/biService";
+
+const biService = new BiService();
 
 export default function DadosVisitas() {
   const [selectedPeriod, setSelectedPeriod] = useState(null);
+  const [usuariosAtivosNumero, setUsuariosAtivosNumero] = useState(null);
+  const [usuarioInativosNumero, setUsuariosInativosNumero] = useState(null);
+
+  useEffect(() => {
+    usuariosAtivos()
+    usuarioInativos()
+  }, [usuariosAtivosNumero,usuarioInativosNumero]);
+
+  const usuariosAtivos = async () => {
+    await biService.usuarioAtivos().then((response) => {
+      setUsuariosAtivosNumero(response.data)
+    })
+  }
+
+  const usuarioInativos = async () => {
+    await biService.usuarioInativos().then((response) => {
+      setUsuariosInativosNumero(response.data)
+    })
+  }
 
   const period = [
     { name: "4 Junho - 31 Julho", code: "NY" },
@@ -53,7 +76,7 @@ export default function DadosVisitas() {
               <h4>Usuarios Ativos</h4>
             </div>
             <div className="col-12 text-center p-2 bg-blue rounded text-white display-6 d-flex align-items-center justify-content-center h-50">
-              <span className="fw-bold">140</span>
+              <span className="fw-bold">{ usuariosAtivosNumero ? usuariosAtivosNumero : 0}</span>
             </div>
           </div>
           <div className="col-12 col-md-6 col-lg-3 px-4 d-flex flex-wrap">
@@ -61,7 +84,7 @@ export default function DadosVisitas() {
               <h4>Usuarios Inativos</h4>
             </div>
             <div className="col-12 text-center p-2 bg-blue rounded text-white display-6 d-flex align-items-center justify-content-center h-50">
-              <span className="fw-bold">140</span>
+              <span className="fw-bold">{ usuarioInativosNumero ? usuarioInativosNumero : 0}</span>
             </div>
           </div>
         </div>
