7beb43882824090982f2fbbd09b3206312ae4c35
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-09 20:38:57 -0300

    FEAT(ChatMessage): Tornando mensagens do usuarios dinamicas

--- unibook/src/components/ChatMessage.jsx
+++ unibook/src/components/ChatMessage.jsx
@@ -4,13 +4,11 @@ export default function ChatMessage(props) {
       <div className="col-12 d-flex justify-content-end">
         <div className="col-8 border border-3 rounded my-2 p-3 border-orange">
           <div className="col-12 d-flex justify-content-end py-1 fw-bold">
-            00/00/0000 00:00 00
+          {props.nome ? props.nome : ""}
           </div>
           <div className="col-12 d-flex">
             <p className="col-11 text-break px-2">
-              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+              {props.mensagem ? props.mensagem : ""}
             </p>
           </div>
         </div>
@@ -21,13 +19,11 @@ export default function ChatMessage(props) {
     <div className="col-12 d-flex justify-content-start">
       <div className="col-8 border border-3 rounded my-2 p-3 border-blue">
         <div className="col-12 d-flex justify-content-end py-1 fw-bold">
-          00/00/0000 00:00 00
+            {props.nome ? props.nome : ""}
         </div>
         <div className="col-12 d-flex">
           <p className="col-11 text-break px-2">
-            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            {props.mensagem ? props.mensagem : ""}
           </p>
         </div>
       </div>
