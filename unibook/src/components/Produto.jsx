fc494bdebbce5ce2e53cc91a45c4c09ca3e9b38b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-09 20:39:47 -0300

    FEAT(Produto): Criando botão para iniciar chat

--- unibook/src/components/Produto.jsx
+++ unibook/src/components/Produto.jsx
@@ -7,29 +7,46 @@ import { Chip } from "primereact/chip";
 import AuthService from "../app/service/authService";
 import CarrinhosService from "../app/service/carrinhosService";
 import { mensagemErro, mensagemSucesso } from "./Toastr";
+import ChatService from "../app/service/chatService";
+
 const service = new CarrinhosService();
+const serviceChat = new ChatService();
 
 export default function Produto(props) {
   const livro = props.livro;
-  let {id} = AuthService.obterUsuarioAutenticado();
+  let { id } = AuthService.obterUsuarioAutenticado();
 
   const comprarProduto = async () => {
     let idLivro = livro.id;
-    let quantidade = 1; 
+    let quantidade = 1;
 
-    service.adicionarProduto(id,idLivro,quantidade).then(
-      () => {
+    service
+      .adicionarProduto(id, idLivro, quantidade)
+      .then(() => {
         mensagemSucesso("Produto adicionado ao carrinho");
-      }
-    ).catch((erro) => {
-      mensagemErro(erro.response.data.message);
-      setTimeout(() => {window.location.href = "/"}, 5000);
-    })
+      })
+      .catch((erro) => {
+        mensagemErro(erro.response.data.message);
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 5000);
+      });
   };
 
   const verPerfilVendedor = () => {
-    window.location.href = `/usuario/${livro.vendedor.id}`
-  }
+    window.location.href = `/usuario/${livro.vendedor.id}`;
+  };
+
+  const iniciarChat = (idVendedor) => {
+    let { id } = JSON.parse(localStorage.getItem("_usuario_logado"));
+      serviceChat.salvarChat(idVendedor, id).then(() => {
+          window.location.href = `/chat`;
+        }).catch(() => {
+          mensagemErro("Erro ao iniciar o chat, tente mais tarde")
+          setTimeout(() => {window.location.href = "/"}, 3000);
+        })
+
+  };
 
   return (
     <div className="col-12 d-flex flex-wrap">
@@ -41,7 +58,7 @@ export default function Produto(props) {
           <h3>{livro.titulo ? livro.titulo : ""}</h3>
           <Divider />
         </div>
-        <div className="col-12">R$ {livro.valor ? livro.valor : 0.00}</div>
+        <div className="col-12">R$ {livro.valor ? livro.valor : 0.0}</div>
         <div className="col-12 py-2">
           <Rating value={4} readOnly stars={5} cancel={false} />
         </div>
@@ -67,30 +84,36 @@ export default function Produto(props) {
             {livro.descricao}
           </p>
           <p>
-            <span className="fw-bold">Ano: </span> {livro.anoPublicacao ? livro.anoPublicacao : '0000'}
+            <span className="fw-bold">Ano: </span>{" "}
+            {livro.anoPublicacao ? livro.anoPublicacao : "0000"}
           </p>
           <p>
-            <span className="fw-bold">Quantidade: </span> {livro.quantidade ? livro.quantidade : '0'}
+            <span className="fw-bold">Quantidade: </span>{" "}
+            {livro.quantidade ? livro.quantidade : "0"}
           </p>
           <p className="fw-bold color-orange">
             *Livros com algumas marcações por dentro
           </p>
         </div>
         <div className="col-12 d-flex justify-content-end align-items-center">
-          {/* Botão Chat */}
           <Button
             icon="pi pi-user"
             label="Ver perfil vendedor"
             className="p-button p-component bg-yellow rounded-pill border border-dark border-2 fw-bold px-4 py-1 text-dark py-2 px-4 me-2 hover-yellow"
             iconPos="right"
-            onClick={() => {verPerfilVendedor()}}
-            /> 
+            onClick={() => {
+              verPerfilVendedor();
+            }}
+          />
           <Button
             icon="pi pi-comments"
             label="Chat"
             className="p-button p-component bg-yellow rounded-pill border border-dark border-2 fw-bold px-4 py-1 text-dark py-2 px-4 me-2 hover-yellow"
             iconPos="right"
-            />
+            onClick={() => {
+              iniciarChat(livro.vendedor.id);
+            }}
+          />
           <Button
             icon="pi pi-shopping-cart"
             className="bg-orange rounded-pill border border-dark text-dark border-2 fw-bold px-4 button-perfil ms-1"
