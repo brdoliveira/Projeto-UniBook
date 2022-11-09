6fda895982df24d222bd3e66172d88e1b7ee3349
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-09 20:40:17 -0300

    FEAT(UsuariosChat): Tornando opções de chat dinamicas

--- unibook/src/components/UsuariosChat.jsx
+++ unibook/src/components/UsuariosChat.jsx
@@ -1,4 +1,4 @@
-export default function UsuariosChat() {
+export default function UsuariosChat(props) {
   return (
     <div className="col-12 d-flex flex-wrap my-3 rounded border border-3 border-orange py-2 px-1 usuarios-chat">
       <div
@@ -6,7 +6,7 @@ export default function UsuariosChat() {
         style={{ width: "5rem", height: "5rem" }}
       ></div>
       <div className="col-8 d-flex align-items-center ps-2 text-white">
-        <h2>Nome Usuario</h2>
+        <h2>{props.nome}</h2>
       </div>
     </div>
   );
