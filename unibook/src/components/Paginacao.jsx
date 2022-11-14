524465327de6d3ced5452458983316189564bb7f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-13 23:07:24 -0300

    FEAT(Paginacao): Tornando paginação dinamica

--- unibook/src/components/Paginacao.jsx
+++ unibook/src/components/Paginacao.jsx
@@ -1,17 +1,23 @@
 import React , { useState } from "react"
+import { Paginator } from 'primereact/paginator';
 
-export default function Paginacao(){
+export default function Paginacao(props){
     const [pagination, setPagination] = useState(0);
-    const [rows, setRows] = useState(10);
+    const [rows, setRows] = useState(12);
 
     const onBasicPageChange = (event) => {
         setPagination(event.first);
         setRows(event.rows);
+        onTrigger()
     }
+
+    function onTrigger() {
+        props.parentCallback(pagination);
+      }
 
     return(
         <div className="col-12 d-flex align-items-center justify-content-center">
-            <Paginator first={pagination} rows={rows} totalRecords={120} rowsPerPageOptions={[10, 20, 30]} onPageChange={onBasicPageChange}></Paginator>
+            <Paginator first={pagination} rows={rows} totalRecords={props.totalRecords} rowsPerPageOptions={[12, 24, 36]} onPageChange={onBasicPageChange}></Paginator>
         </div>  
     )
 }
