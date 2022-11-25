21dfe9c194c4216cc74c3919fb2fc24c65434783
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-25 02:15:27 -0300

    FEAT(chatService): Recriando endpoint

--- unibook/src/app/service/chatService.js
+++ unibook/src/app/service/chatService.js
@@ -2,32 +2,32 @@ import ApiService from "../apiservice";
 
 class ChatService extends ApiService{
     constructor(){
-        super('/chat')
+        super('')
     }
-
-    buscarChat(id){
-        return this.get(`?id=${id}`)
-    }
-
+    
     enviarMensagem(idChat, idAnuncioProduto, idUsuarioRemetente, mensagem){
-        return this.postObject('/enviar-mensagem', {
+        return this.postObject('/envia-mensagem', {
             "idChat": idChat,
             "idAnuncioProduto": idAnuncioProduto,
             "idUsuarioRemetente": idUsuarioRemetente,
             "mensagem": mensagem
         })
     }
+    
+    buscarChat(id){
+        return this.get(`/chat?id=${id}`)
+    }
 
     listarChat(idUsuario){
-        return this.get(`/usuario?idusuario=${idUsuario}`)
+        return this.get(`/chat/usuario?idusuario=${idUsuario}`)
     }
 
     buscarMensagens(idUsuarioRemetente,idAnuncioProduto){
-        return this.get(`/mensagens?idUsuarioRemetente=${idUsuarioRemetente}&idAnuncioProduto=${idAnuncioProduto}`)
+        return this.get(`/chat/mensagens?idUsuarioRemetente=${idUsuarioRemetente}&idAnuncioProduto=${idAnuncioProduto}`)
     }
     
     salvarChat(idUsuarioRemetente,idAnuncioProduto){
-        return this.get(`/mensagens?idUsuarioRemetente=${idUsuarioRemetente}&idAnuncioProduto=${idAnuncioProduto}`)
+        return this.get(`/chat/mensagens?idUsuarioRemetente=${idUsuarioRemetente}&idAnuncioProduto=${idAnuncioProduto}`)
     }
 
 
