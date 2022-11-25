b99ed3e7c455aa375e57ac20d237577371eeed2b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-24 23:57:45 -0300

    FEAT(ComentarioVazio): Criando componente de campo de comentario vazio

---
+++ unibook/src/components/ComentarioVazio.jsx
@@ -0,0 +1,8 @@
+
+export default function ComentarioVazio(){
+    return(
+        <div className="col-12 d-flex align-items-center justify-content-center bg-secondary py-4 rounded">
+            <h4 className="py-4 fw-bold text-white">Nenhum comentário encontrado</h4>
+        </div>
+    )
+}
