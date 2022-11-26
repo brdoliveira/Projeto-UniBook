0a20f4441a53820791f9f801ad2da9f87fc98757
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-26 19:18:51 -0300

    FEAT(PagePerfil): Adicionando verificação de abas

--- unibook/src/pages/PagePerfil.jsx
+++ unibook/src/pages/PagePerfil.jsx
@@ -7,6 +7,15 @@ import PerfilUsuario from "../components/PerfilUsuario";
 import PerfilVendedor from "../components/PerfilVendedor";
 import PerfilAnalitycs from "../components/PerfilAnalytics";
 class PagePerfil extends React.Component {
+  constructor(){
+    super()
+    this.state = ""
+  }
+
+  componentDidMount(){
+    this.setState(JSON.parse(localStorage.getItem("_usuario_logado")));
+  }
+
   render() {
     return (
       <div className="min-vh-100">
@@ -19,10 +28,10 @@ class PagePerfil extends React.Component {
               <TabPanel header="Usuario">
                 <PerfilUsuario className="col-12" />
               </TabPanel>
-              <TabPanel header="Vendedor">
+              <TabPanel header="Vendedor" disabled={this.state.tipoUsuarip === "VENDEDOR"}>
                 <PerfilVendedor className="col-12" />
               </TabPanel>
-              <TabPanel header="Analytics">
+              <TabPanel header="Analytics" disabled={this.state.tipoUsuarip === "VENDEDOR"}>
                 <PerfilAnalitycs className="col-12" />
               </TabPanel>
             </TabView>
