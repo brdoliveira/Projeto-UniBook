2f6c020792ae1803b8a77329cf8cb852a464e1a1
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-26 23:33:33 -0300

    FEAT(PageChat): Alinhando chat na pagina

--- unibook/src/pages/PageChat.jsx
+++ unibook/src/pages/PageChat.jsx
@@ -8,17 +8,16 @@ import "../templates/styles/styles-perfil.css";
 class PageChat extends React.Component {
   render() {
     return (
-        <>
+        <div className="min-vh-100">
           <Menu />
           <div
-            className="col-12 d-flex justify-content-center align-items-center"
-            style={{ height: "90vh" }}
+            className="col-12 d-flex justify-content-center align-items-center pt-4"
           >
-            <div className="col-10">
+            <div className="col-10 d-flex justify-content-center align-items-center pt-4 mt-4">
                 <Chat/>
             </div>
           </div>
-        </>
+        </div>
       );
   }
 }
