12d84893ea8630745802a13be53aa409613ed11a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-27 00:01:13 -0300

    FEAT(RemoverEditarProdutoItem): Tornando item dinamico

--- unibook/src/components/RemoverEditarProdutoItem.jsx
+++ unibook/src/components/RemoverEditarProdutoItem.jsx
@@ -1,15 +1,14 @@
-import React , { useState } from "react";
+import React from "react";
 
 import { Button } from "primereact/button";
 
 import { ConfirmDialog , confirmDialog } from 'primereact/confirmdialog';
 
 export default function RemoverEditarProdutoItem(props) {
-  const [produto] = useState(props.produto)
 
   const confirm = () => {
     confirmDialog({
-        message: `Tem certeza que deseja deletar o produto ${produto.nome}?`,
+        message: `Tem certeza que deseja deletar o produto ${props.produto.titulo}?`,
         header: 'Confirmação',
         icon: 'pi pi-exclamation-triangle',
         accept: () => console.log("Produto deletado !"),
@@ -21,13 +20,13 @@ export default function RemoverEditarProdutoItem(props) {
     <>
       <div className="col-12 rounded bg-white my-2 d-flex flex-wrap p-2" style={{ height: "7rem" }}>
         <div className="col-12 col-lg-5">
-            <span className="fw-bold">Nome: </span>
+            <span className="fw-bold">Nome: {props.produto.titulo}</span>
         </div>
         <div className="col-6 col-lg-3">
-          <span className="fw-bold">Editora: </span>
+          <span className="fw-bold">Autor: {props.produto.autor} </span>
         </div>
         <div className="col-6 col-lg-2">
-            <span className="fw-bold">Ano: </span>
+            <span className="fw-bold">Ano: {props.produto.anoPublicacao}</span>
         </div>
         <div className="col-12 col-lg-2 d-flex align-items-center justify-content-end">
           <Button icon="pi pi-pencil" className="rounded-pill p-button-secondary"/>
