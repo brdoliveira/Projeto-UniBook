2323d140b087f2c481b12fead236127ec359c04f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-26 23:58:30 -0300

    FEAT(PerfilVendedor): Adicionando foto do usuario

--- unibook/src/components/PerfilVendedor.jsx
+++ unibook/src/components/PerfilVendedor.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import FeedbackMessage from "./FeedbackMessage";
 import EnviarFeedback from "./EnviarFeedback";
 import ComponenteVazioHome from "./ComponenteVazioHome";
 import CardProduto from "./CardProduto";
@@ -13,6 +12,7 @@ import { ScrollPanel } from "primereact/scrollpanel";
 
 import "../templates/styles/styles-perfil.css";
 import ProdutosAnunciadosService from "../app/service/produtosAnunciadosService";
+import ComponenteItemsVazio from "./ComponenteItemsVazio";
 
 class PerfilVendedor extends React.Component {
   constructor(){
@@ -22,6 +22,7 @@ class PerfilVendedor extends React.Component {
 
   state = {
     nome: "",
+    foto: "",
     dataNascimento: "",
     sexo: "",
     cpf: "",
@@ -62,12 +63,12 @@ class PerfilVendedor extends React.Component {
   render() {
     return (
       <div className="col-12 d-flex flex-wrap">
-        <div className="col-12 col-lg-6 bg-orange div-perfil rounded d-flex flex-wrap align-items-center justify-content-center py-4 h-100">
+        <div className="col-12 col-lg-6 bg-orange div-perfil rounded d-flex flex-wrap align-items-center justify-content-center py-4">
           <div
             className="col-12 bg-white rounded-pill d-flex flex-wrap align-items-center justify-content-center"
             style={{ height: "300px", width: "300px" }}
           >
-            <img alt="" />
+            <img className="rounded-pill" alt={this.state.foto ? this.state.foto : ""} style={{ height: "300px", width: "300px" }} src={this.state.foto ? this.state.foto : ""} />
           </div>
           <span className="col-12 text-center text-dark py-2">
           <span className="fs-3" style={{textTransform: 'capitalize'}}>
@@ -81,27 +82,21 @@ class PerfilVendedor extends React.Component {
             </span>
           </span>
         </div>
-        <div className="col-12 col-lg-6 flex-wrap align-items-start px-2">
-          <div className="col-12 bg-blue text-white my-2 rounded">
+        <div className="col-12 col-lg-6 div-perfil flex-wrap align-items-start px-2">
+          <div className="col-12 bg-blue text-white rounded h-100">
             <div className="col-12 d-flex align-items-center justify-content-center py-2">
               <div className="col-11 bg-white rounded py-2 px-2 d-flex">
-                <Rating value={3} readOnly stars={5} cancel={false} />
-                <span className="px-2 text-dark">1384 Avaliações</span>
+                <Rating value={5} readOnly stars={5} cancel={false} />
+                <span className="px-2 text-dark">0 Avaliações</span>
               </div>
             </div>
             <div className="col-12 d-flex align-items-center justify-content-center rounded flex-wrap">
               <ScrollPanel
-                className="col-11 scroll text-dark bg-white rounded mb-3 ps-1 justify-content-center flex-wrap"
+                className="col-11 scroll text-dark bg-white rounded mb-3 ps-2 pt-2 justify-content-center flex-wrap"
                 style={{ height: "30vh" }}
               >
-                <FeedbackMessage />
-                <FeedbackMessage />
-                <FeedbackMessage />
-                <FeedbackMessage />
-                <FeedbackMessage />
-                <FeedbackMessage />
-                <FeedbackMessage />
-                <FeedbackMessage />
+                <ComponenteItemsVazio mensagem="Nenhum feedback encontrado"/>
+                {/* <FeedbackMessage /> */}
               </ScrollPanel>
               <div className="col-11 rounded">
                 <EnviarFeedback />
