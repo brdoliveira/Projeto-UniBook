5793d7afa2621e6d749e76eb994919d6f3a14f1d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-27 00:00:48 -0300

    FEAT(RemoverEditarProduto): Criando parte de editar ou remover produto

--- unibook/src/components/RemoverEditarProduto.jsx
+++ unibook/src/components/RemoverEditarProduto.jsx
@@ -1,21 +1,47 @@
-import { ScrollPanel } from 'primereact/scrollpanel';
+import React from "react";
+
+import { ScrollPanel } from "primereact/scrollpanel";
 
 import RemoverEditarProdutoItem from "./RemoverEditarProdutoItem";
+import ProdutosAnunciadosService from "../app/service/produtosAnunciadosService";
 
-export default function RemoverEditarProduto(){
-    return(
-        <>
-        <div className="col-12 rounded bg-blue d-flex justify-content-center align-items-center flex-wrap" style={{ height: "30rem" }}>
-            <div className="col-11 text-white pt-4">
-                <h2>Remover Produto</h2>
-            </div>
-            <ScrollPanel className="col-11 bg-bluelight pb-4 rounded py-4 ps-3" style={{ height: "20rem" }}>
-                <RemoverEditarProdutoItem/>
-                <RemoverEditarProdutoItem/>
-                <RemoverEditarProdutoItem/>
-                <RemoverEditarProdutoItem/>
-            </ScrollPanel>
+class RemoverEditarProduto extends React.Component {
+  constructor() {
+    super();
+    this.service = new ProdutosAnunciadosService();
+    this.state = {
+      produtos: [],
+    };
+  }
+
+  componentDidMount() {
+    this.service.listarTodosPilha().then((response) => {
+      this.setState({ produtos: response.data.pilha });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          className="col-12 rounded bg-blue d-flex justify-content-center align-items-center flex-wrap"
+          style={{ height: "30rem" }}
+        >
+          <div className="col-11 text-white pt-4">
+            <h2>Remover/Editar Produto</h2>
+          </div>
+          <ScrollPanel
+            className="col-11 bg-bluelight pb-4 rounded py-4 ps-3"
+            style={{ height: "20rem" }}
+          >
+            {this.state.produtos.map((item,idx) => {
+              return <RemoverEditarProdutoItem produto={item} key={idx + 19000} />;
+            })}
+          </ScrollPanel>
         </div>
-        </>
-    )
+      </>
+    );
+  }
 }
+
+export default RemoverEditarProduto;
