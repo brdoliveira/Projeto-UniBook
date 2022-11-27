e9f3a8b7b1a3231bdb745dd80f1b0611d91e6131
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-26 23:55:38 -0300

    FEAT(Chat): Adicionando validações e adicionando componente de mensagem vazio

--- unibook/src/components/Chat.jsx
+++ unibook/src/components/Chat.jsx
@@ -6,6 +6,8 @@ import ChatMessage from "./ChatMessage";
 import { ScrollPanel } from "primereact/scrollpanel";
 import EnviarMensagem from "./EnviarMensagem";
 import ChatService from "../app/service/chatService";
+import { mensagemErro } from "./Toastr";
+import ComponenteItemsVazio from "./ComponenteItemsVazio";
 
 class Chat extends React.Component {
   constructor() {
@@ -16,13 +18,13 @@ class Chat extends React.Component {
       chat: [],
       idAnuncioProduto: 0,
       idUsuario: 0,
-      idChat: 0
+      idChat: 0,
     };
   }
 
   async componentDidMount() {
-    let { id } = JSON.parse(localStorage.getItem("_usuario_logado"))
-    this.setState({"idUsuario": id });
+    let { id } = JSON.parse(localStorage.getItem("_usuario_logado"));
+    this.setState({ idUsuario: id });
 
     await this.listaChat(id);
     this.buscarMensagens(id, this.state.idAnuncioProduto);
@@ -30,36 +32,40 @@ class Chat extends React.Component {
 
   async listaChat(idUsuario) {
     await this.service.listarChat(idUsuario).then((response) => {
-      console.log(response);
       this.setState({
         users: response.data.map((chat) => ({
           nome: chat.anuncioProduto.vendedor.nome,
           idAnuncio: chat.anuncioProduto.id,
         })),
         idAnuncioProduto: response.data[0]["anuncioProduto"]["id"],
-        idChat: response.data[0]["id"]
+        idChat: response.data[0]["id"],
       });
     });
   }
 
   buscarMensagens(idUsuario, idAnuncio) {
-    this.service.buscarMensagens(idUsuario, idAnuncio).then((response) => {
-      this.setState({ chat: response.data.mensagemLista });
-    });
+    this.service
+      .buscarMensagens(idUsuario, idAnuncio)
+      .then((response) => {
+        this.setState({ chat: response.data.mensagemLista });
+      })
+      .catch(() => {
+        mensagemErro("Nenhuma mensagem encontrada")
+      });
   }
 
   handleCallback = (childData) => {
     this.setState({
-      idAnuncioProduto : childData 
+      idAnuncioProduto: childData,
     });
-    this.buscarMensagens(this.state.idUsuario, this.state.idAnuncioProduto)
+    this.buscarMensagens(this.state.idUsuario, this.state.idAnuncioProduto);
   };
 
   render() {
     return (
       <div
         className="col-12 d-flex flex-wrap bg-blue py-4 rounded"
-        style={{ height: "60vh" }}
+        style={{ height: "40rem" }}
       >
         <div className="col-12 text-center text-white">
           <h1>Chat</h1>
@@ -67,9 +73,11 @@ class Chat extends React.Component {
         <div className="col-4 d-flex align-items-center justify-content-center">
           <ScrollPanel
             className="col-11 scroll text-dark mb-3 ps-1 justify-content-center flex-wrap"
-            style={{ height: "50vh" }}
+            style={{ height: "40rem" }}
           >
-            {this.state.users.map((chat) => {
+            {
+            this.state.usuario ?
+            this.state.users.map((chat) => {
               return (
                 <UsuariosChat
                   parentCallback={this.handleCallback}
@@ -78,27 +86,29 @@ class Chat extends React.Component {
                   nome={chat.nome}
                 />
               );
-            })}
+            }) : <ComponenteItemsVazio mensagem="Nenhum usuario encontrado"/>  
+            }
           </ScrollPanel>
         </div>
-        <div className="col-8 py-2 d-flex align-items-center justify-content-center flex-wrap">
+        <div
+          className="col-8 py-2 d-flex align-items-center justify-content-center flex-wrap"
+          style={{ height: "30rem" }}
+        >
           <ScrollPanel
             className="col-11 scroll text-dark bg-white rounded mb-3 ps-1 justify-content-center flex-wrap"
-            style={{ height: "40vh" }}
+            style={{ height: "25rem" }}
           >
-            {this.state.chat.map((chat) => {
+            {this.state.chat ? this.state.chat.map((chat) => {
               return (
                 <ChatMessage
                   proprietarioMensagem={
-                    chat.usuario.id === this.state.idUsuario
-                      ? true
-                      : false
+                    chat.usuario.id === this.state.idUsuario ? true : false
                   }
                   mensagem={chat.mensagem}
                   nome={chat.usuario.nome}
                 />
               );
-            })}
+            }) : <ComponenteItemsVazio mensagem="Nenhuma mensagem encontrada"/> }
           </ScrollPanel>
           <div className="col-12 d-flex justify-content-center">
             <EnviarMensagem
