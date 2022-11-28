74c9c6df7f8108085c948b2d5cab5b554787052d
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-27 23:42:45 -0300

    FEAT(UsuariosChat): Adicionando foto e adicionando botão para ver produto

--- unibook/src/components/UsuariosChat.jsx
+++ unibook/src/components/UsuariosChat.jsx
@@ -1,19 +1,38 @@
+import { Button } from "primereact/button";
 import React from "react";
 
 export default function UsuariosChat(props) {
-
-  const setIdAnuncioProduto = () =>{
-    props.parentCallback(props.idAnuncio)
-  }
+  const setIdAnuncioProduto = () => {
+    props.parentCallback(props.idAnuncio);
+  };
 
   return (
-    <div className="col-12 d-flex flex-wrap my-3 rounded border border-3 border-orange py-2 px-1 usuarios-chat" onClick={() => { setIdAnuncioProduto()}}>
+    <div
+      className="col-12 d-flex flex-wrap my-3 rounded border border-3 border-orange py-2 px-1 usuarios-chat"
+      onClick={() => {
+        setIdAnuncioProduto();
+      }}
+    >
       <div
         className="col-4 rounded-pill bg-white"
         style={{ width: "5rem", height: "5rem" }}
-      ></div>
+      >
+        <img
+          src={props.foto}
+          alt={props.foto}
+          className="img-fluid rounded-pill"
+          style={{ width: "5rem", height: "5rem" }}
+        />
+      </div>
       <div className="col-8 d-flex align-items-center ps-2 text-white">
-        <h2>{props.nome}</h2>
+        <h5>{props.nome ? props.nome.slice(0, 13) + "..." : ""}</h5>
+        <Button
+          className="p-1"
+          label="Ver produto"
+          onClick={() => {
+            window.location.href = `/produto/${props.idAnuncio}`;
+          }}
+        />
       </div>
     </div>
   );
