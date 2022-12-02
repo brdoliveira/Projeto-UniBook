99cb5842548f2acbf4df49b254144dc07f986b8e
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-01 21:26:22 -0300

    FEAT(AdmnistradorAnalytics): Tornando tela dinamica

--- unibook/src/components/AdministradorAnalytics.jsx
+++ unibook/src/components/AdministradorAnalytics.jsx
@@ -1,35 +1,23 @@
 import React from "react";
 
-import TabelaAdministrador from "./TabelaAdministrador";
+import TabelaAdministradorVolumeVendas from "./TabelaAdministradorVolumeVendas";
 
 import MapaBrasil from "./MapaBrasil";
-import GraficoBarra from "./GraficoBarra";
+import GraficoBarraQuantidadeVendasRegiao from "./GraficoBarraQuantidadeVendasRegiao";
 import DadosVisitas from "./DadosVisitas";
 import GraficoBarraAtivosInativos from "./GraficoBarraAtivosInativos";
 import BiService from "../app/service/biService";
+import TabelaAdministradorQuantidadeVendasRegiao from "./TabelaAdministradorQuantidadeVendasRegiao";
 
 class AdministradorAnalytics extends React.Component {
   constructor() {
     super();
     this.service = new BiService();
-    this.state = {
-      usuariosAtivos: 0,
-      usuariosInativos: 0,
-    };
   }
 
   componentDidMount() {
   }
 
-  async usuarioAtivosInativos() {
-    await this.service.usuarioAtivos().then((res) => {
-      this.setState({ usuariosAtivos: res.data });
-    });
-
-    await this.service.usuarioInativos().then((res) => {
-      this.setState({ usuariosInativos: res.data });
-    });
-  }
 
   render() {
     return (
@@ -47,22 +35,25 @@ class AdministradorAnalytics extends React.Component {
           <GraficoBarraAtivosInativos/>
         </div>
         <div className="col-12 text-dark">
-          <h2>Mapa Geográfico Volume de Compras</h2>
+          <h2>Mapa Geográfico Quantidade de Compras</h2>
         </div>
         <div className="col-12 py-4">
           <MapaBrasil />
         </div>
+        <div className="col-12 text-dark">
+          <h2>Valor Monetário de Compras por Estado</h2>
+        </div>
         <div className="col-12 py-4">
-          <TabelaAdministrador />
+          <TabelaAdministradorVolumeVendas />
         </div>
         <div className="col-12 text-dark">
           <h2>Volume de Vendas por Mês</h2>
         </div>
         <div className="col-12 py-4">
-          <GraficoBarra />
+          <GraficoBarraQuantidadeVendasRegiao />
         </div>
         <div className="col-12 py-4">
-          <TabelaAdministrador />
+          <TabelaAdministradorQuantidadeVendasRegiao />
         </div>
       </>
     );
