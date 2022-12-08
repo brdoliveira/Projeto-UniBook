aa002a2bef9b4e60b00d1807330317fe28aab74f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-08 00:31:12 -0300

    FEAT(apiservice): Alterando porta

--- unibook/src/app/apiservice.js
+++ unibook/src/app/apiservice.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    // baseURL: "http://localhost:8080/"
+    // baseURL: "http://localhost:8004/"
     baseURL: "http://54.82.168.80:8004"
     // baseURL: "http://unibook.ddns.net:8004"
 })
-
 
 class ApiService{
     constructor(apiUrl){
